00629b486d13faa88e698acebe7c7d1307ab1ddb
Diego Rondón <furrtr4532@gmail.com>
2024-10-18 09:04:40 -0500

    feat: agregar servicio de cliente en el API

---
+++ src/redux/services/clientApi.ts
@@ -0,0 +1,22 @@
+import { ClientData } from "@/types";
+import { createApi } from "@reduxjs/toolkit/query/react";
+
+import baseQueryWithReauth from "./baseQuery";
+
+export const adminApi = createApi({
+  reducerPath: "clientProfileApi",
+  baseQuery: baseQueryWithReauth,
+  tagTypes: ["Auth Client"],
+  endpoints: (build) => ({
+    profile: build.query<ClientData, void>({
+      query: () => ({
+        url: "/auth/client/profile",
+        credentials: "include",
+      }),
+
+      providesTags: ["Auth Client"],
+    }),
+  }),
+});
+
+export const { useProfileQuery } = adminApi;
