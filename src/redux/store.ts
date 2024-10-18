dc079511b14116bbe3d3becf719d4f70f121e1a7
EleoRomario <eleo.romario@gmail.com>
2024-10-18 16:06:07 -0500

    feat: agregar servicio categoryApi y configuración en el store
    Se agregó el servicio categoryApi para obtener categorías y se configuró en el store para su uso.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
+import { categoryApi } from "./services/categoryApi";
+
 export const store = configureStore({
-  reducer: {},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({}),
+  reducer: {
+    [categoryApi.reducerPath]: categoryApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({}).concat(categoryApi.middleware),
 });
 
 setupListeners(store.dispatch);
