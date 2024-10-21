5f9658ba3e9b04065469304b3579b75ffce9c7e3
EleoRomario <eleo.romario@gmail.com>
2024-10-21 12:01:04 -0500

    feat: agregar componente MerchItem para mostrar productos recomendados en la página de inicio

--- src/components/home/Featured.tsx
+++ src/components/home/Featured.tsx
@@ -1,4 +1,8 @@
+"use client";
+import { useCatalog } from "@/hooks/use-catalog";
+
 import { CartSkeleton } from "../cart/CartSkeleton";
+import { MerchItem } from "../cart/MerchItem";
 
 interface FeaturedHeaderProps {
   children: React.ReactNode;
@@ -7,8 +11,14 @@ interface FeaturedHeaderProps {
 const FeaturedHeader: React.FC<FeaturedHeaderProps> = ({ children }) => {
   return (
     <section className="flex flex-col gap-20">
-      <h2 className="text-center text-3xl font-semibold">
-        Productos destacados
+      <h2 className="flex flex-col gap-2 text-center text-3xl font-semibold">
+        <span className="text-balance">
+          Productos especiales de{" "}
+          <span className="font-black uppercase text-secondary">chaqchao</span>
+        </span>
+        <p className="text-balance text-lg">
+          ¡Vea nuestros productos especiales que puede obtener!
+        </p>
       </h2>
       <div className="flex gap-6">{children}</div>
     </section>
@@ -16,10 +26,22 @@ const FeaturedHeader: React.FC<FeaturedHeaderProps> = ({ children }) => {
 };
 
 export const Featured = () => {
+  const { productRecommend, isLoadingProductRecommend } = useCatalog();
+
+  if (isLoadingProductRecommend || !productRecommend) {
+    return (
+      <FeaturedHeader>
+        {[...Array(4)].map((_, index) => (
+          <CartSkeleton key={index} />
+        ))}
+      </FeaturedHeader>
+    );
+  }
+
   return (
     <FeaturedHeader>
-      {[...Array(4)].map((_, index) => (
-        <CartSkeleton key={index} />
+      {productRecommend.map((product, index) => (
+        <MerchItem key={index} product={product} />
       ))}
     </FeaturedHeader>
   );
