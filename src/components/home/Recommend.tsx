3817a101cf4c3bf9120d97e7a83fde9131c22857
EleoRomario <eleo.romario@gmail.com>
2024-10-21 09:10:52 -0500

    feat: actualizar componente Recommend para mostrar productos recomendados en la página de inicio

--- src/components/home/Recommend.tsx
+++ src/components/home/Recommend.tsx
@@ -2,8 +2,7 @@
 import { useCatalog } from "@/hooks/use-catalog";
 
 import { CartItem } from "../cart/CartItem";
-import { Card } from "../ui/card";
-import { Skeleton } from "../ui/skeleton";
+import { CartSkeleton } from "../cart/CartSkeleton";
 
 interface RecommendHeaderProps {
   children: React.ReactNode;
@@ -26,13 +25,7 @@ export const Recommend = () => {
     return (
       <RecommendHeader>
         {[...Array(4)].map((_, index) => (
-          <Card key={index} className="h-auto w-60">
-            <div className="flex flex-col items-center justify-center gap-4 px-2 py-10">
-              <Skeleton className="size-32 rounded"></Skeleton>
-              <Skeleton className="h-20 w-48 rounded"></Skeleton>
-            </div>
-            <Skeleton className="m-4 h-10"></Skeleton>
-          </Card>
+          <CartSkeleton key={index} />
         ))}
       </RecommendHeader>
     );
