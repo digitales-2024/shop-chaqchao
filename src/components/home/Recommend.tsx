c6ed95fa7014d564cc6812a935c85df84f14a172
EleoRomario <eleo.romario@gmail.com>
2024-10-21 08:41:53 -0500

    feat: agregar componente Skeleton para mostrar carga de contenido en la sección de recomendados

--- src/components/home/Recommend.tsx
+++ src/components/home/Recommend.tsx
@@ -2,6 +2,8 @@
 import { useCatalog } from "@/hooks/use-catalog";
 
 import { CartItem } from "../cart/CartItem";
+import { Card } from "../ui/card";
+import { Skeleton } from "../ui/skeleton";
 
 export const Recommend = () => {
   const { productRecommend, isLoadingProductRecommend } = useCatalog();
@@ -13,22 +15,13 @@ export const Recommend = () => {
         </h2>
         <div className="flex gap-6">
           {[...Array(4)].map((_, index) => (
-            <div
-              key={index}
-              className="h-auto w-60 animate-pulse border border-gray-300 shadow-none transition-all duration-500 hover:shadow-md"
-            >
+            <Card key={index} className="h-auto w-60">
               <div className="flex flex-col items-center justify-center gap-4 px-2 py-10">
-                <div className="h-20 w-48 rounded bg-gray-300"></div>
-                <div className="grid h-full grid-rows-[auto,1fr,30px] gap-4 text-center">
-                  <div className="h-4 w-full rounded bg-gray-300"></div>
-                  <div className="h-6 w-3/4 self-center rounded bg-gray-300"></div>
-                  <div className="h-6 w-1/2 self-center rounded bg-gray-300"></div>
-                </div>
+                <Skeleton className="size-32 rounded"></Skeleton>
+                <Skeleton className="h-20 w-48 rounded"></Skeleton>
               </div>
-              <div className="border-t border-gray-300 p-3">
-                <div className="h-10 rounded bg-gray-300"></div>
-              </div>
-            </div>
+              <Skeleton className="m-4 h-10"></Skeleton>
+            </Card>
           ))}
         </div>
       </section>
@@ -41,7 +34,7 @@ export const Recommend = () => {
 
   return (
     <section className="flex flex-col gap-20">
-      <h2 className="text-center text-3xl font-semibold">
+      <h2 className="mb-20 text-center text-3xl font-semibold">
         Recomendados para ti
       </h2>
       <div className="flex gap-6">
