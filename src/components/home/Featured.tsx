99334716be39e9c616e6f3df74921831c1d7003a
EleoRomario <eleo.romario@gmail.com>
2024-10-21 09:08:53 -0500

    feat: agregar componente CartSkeleton para mostrar carga de contenido en la sección de carrito

--- src/components/home/Featured.tsx
+++ src/components/home/Featured.tsx
@@ -1,7 +1,26 @@
+import { CartSkeleton } from "../cart/CartSkeleton";
+
+interface FeaturedHeaderProps {
+  children: React.ReactNode;
+}
+
+const FeaturedHeader: React.FC<FeaturedHeaderProps> = ({ children }) => {
+  return (
+    <section className="flex flex-col gap-20">
+      <h2 className="text-center text-3xl font-semibold">
+        Productos destacados
+      </h2>
+      <div className="flex gap-6">{children}</div>
+    </section>
+  );
+};
+
 export const Featured = () => {
   return (
-    <section>
-      <h2 className="text-2xl font-bold">Productos destacados</h2>
-    </section>
+    <FeaturedHeader>
+      {[...Array(4)].map((_, index) => (
+        <CartSkeleton key={index} />
+      ))}
+    </FeaturedHeader>
   );
 };
