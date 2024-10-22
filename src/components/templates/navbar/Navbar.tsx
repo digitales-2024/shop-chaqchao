2dafae37ba78484a92976a5f59024cd19c9e8074
EleoRomario <eleo.romario@gmail.com>
2024-10-22 17:11:55 -0500

    feat: actualizar barra de navegación con componente MenuList y actualizar icono del carrito

--- src/components/templates/navbar/Navbar.tsx
+++ src/components/templates/navbar/Navbar.tsx
@@ -1,19 +1,24 @@
+"use client";
 import { ChaqchaoCharacter } from "@/assets/images/ChaqchaoCharacter";
 import { ChaqchaoName } from "@/assets/images/ChaqchaoName";
 import Link from "next/link";
-import { Suspense } from "react";
 
 import { CartSheet } from "@/components/cart/CartSheet";
 
+import { cn } from "@/lib/utils";
+
+import { MenuList } from "./MenuList";
 import { SearchBar } from "./SearchBar";
 import { UserLogin } from "./UserLogin";
 
-export async function Navbar() {
+export function Navbar() {
   return (
-    <nav className="relative flex items-center justify-between p-4 lg:px-6">
-      <div className="block flex-none md:hidden">
-        <Suspense fallback={null}>{/* <MobileMenu menu={menu} /> */}</Suspense>
-      </div>
+    <nav
+      className={cn(
+        "fixed z-50 flex w-full items-center justify-between bg-white p-4 lg:px-6",
+        {},
+      )}
+    >
       <div className="grid w-full grid-cols-3 items-center justify-center">
         <div className="flex w-full">
           <Link
@@ -24,28 +29,14 @@ export async function Navbar() {
             <ChaqchaoCharacter className="h-24" />
             <ChaqchaoName className="w-48" />
           </Link>
-          {/* {menu.length ? (
-            <ul className="hidden gap-6 text-sm md:flex md:items-center">
-              {menu.map((item: Menu) => (
-                <li key={item.title}>
-                  <Link
-                    href={item.path}
-                    prefetch={true}
-                    className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
-                  >
-                    {item.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : null} */}
         </div>
-        <div className="flex justify-center">
+        <div className="inline-flex w-full justify-center">
+          <MenuList />
+        </div>
+        <div className="flex items-center justify-end gap-4">
           <SearchBar />
-        </div>
-        <div className="flex justify-end gap-4">
-          <UserLogin />
           <CartSheet />
+          <UserLogin />
         </div>
       </div>
     </nav>
