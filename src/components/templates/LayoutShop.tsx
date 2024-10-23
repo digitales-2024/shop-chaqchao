9c14f75c8a4200a95562d36d4bc78d547fc576ab
EleoRomario <eleo.romario@gmail.com>
2024-10-23 16:35:47 -0500

    refactor: refactorizar LayoutShop eliminando código innecesario

--- src/components/templates/LayoutShop.tsx
+++ src/components/templates/LayoutShop.tsx
@@ -1,5 +1,3 @@
-"use client";
-import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
 import { ScrollArea } from "../ui/scroll-area";
@@ -10,29 +8,7 @@ interface LayoutShopProps {
   children: ReactNode;
 }
 
-// Arreglo con las rutas que no necesitan autorización
-const noAuthRoutes = [
-  "/sign-in",
-  "/register",
-  "/forgot-password",
-  "/terms",
-  "/reset-password",
-];
-
 export function LayoutShop({ children }: LayoutShopProps) {
-  const pathname = usePathname();
-
-  // Verifica si la ruta actual está en el arreglo de rutas que no necesitan autorización
-  const isNoAuthRoute = noAuthRoutes.includes(pathname);
-
-  if (isNoAuthRoute) {
-    return (
-      <>
-        <main>{children}</main>
-      </>
-    );
-  }
-
   return (
     <ScrollArea className="h-svh">
       <Navbar />
