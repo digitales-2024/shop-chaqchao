43efa5723d03ca74cbe77b0737ba3b53aec165cf
EleoRomario <eleo.romario@gmail.com>
2024-10-23 16:36:00 -0500

    feat: agregar selector de idioma en la barra de navegación

--- src/components/templates/navbar/Navbar.tsx
+++ src/components/templates/navbar/Navbar.tsx
@@ -1,25 +1,20 @@
-"use client";
 import { ChaqchaoCharacter } from "@/assets/images/ChaqchaoCharacter";
 import { ChaqchaoName } from "@/assets/images/ChaqchaoName";
+import { useLocale } from "next-intl";
 import Link from "next/link";
 
 import { CartSheet } from "@/components/cart/CartSheet";
 
-import { cn } from "@/lib/utils";
-
+import { LanguageSelector } from "./LanguageSelector";
 import { MenuList } from "./MenuList";
 import { SearchBar } from "./SearchBar";
 import { UserLogin } from "./UserLogin";
 
 export function Navbar() {
+  const locale = useLocale();
   return (
-    <nav
-      className={cn(
-        "fixed z-50 flex w-full items-center justify-between bg-white p-4 lg:px-6",
-        {},
-      )}
-    >
-      <div className="grid w-full grid-cols-3 items-center justify-center">
+    <nav className="fixed z-50 flex w-full items-center justify-between bg-white p-4 lg:px-6">
+      <div className="container mx-auto grid w-full grid-cols-3 items-center justify-center">
         <div className="flex w-full">
           <Link
             href="/"
@@ -30,13 +25,14 @@ export function Navbar() {
             <ChaqchaoName className="w-48" />
           </Link>
         </div>
-        <div className="inline-flex w-full justify-center">
+        <div className="inline-flex justify-center">
           <MenuList />
         </div>
-        <div className="flex items-center justify-end gap-4">
+        <div className="flex items-center justify-end gap-6">
           <SearchBar />
           <CartSheet />
           <UserLogin />
+          <LanguageSelector defaultValue={locale} />
         </div>
       </div>
     </nav>
