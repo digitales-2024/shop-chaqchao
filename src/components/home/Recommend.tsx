25b73f0a7e77e39f5fde9da2c7f9462fd01dc9ec
EleoRomario <eleo.romario@gmail.com>
2024-10-25 15:04:01 -0500

    refactor: actualizar estilos de la sección destacada

--- src/components/home/Recommend.tsx
+++ src/components/home/Recommend.tsx
@@ -4,6 +4,7 @@ import { useCatalog } from "@/hooks/use-catalog";
 import { CartItem } from "../cart/CartItem";
 import { CartSkeleton } from "../cart/CartSkeleton";
 import { BusinessSchedule } from "../common/Schedule";
+import { SeparateSection } from "../common/SeparateSection";
 
 interface RecommendHeaderProps {
   children: React.ReactNode;
@@ -11,20 +12,24 @@ interface RecommendHeaderProps {
 
 const RecommendHeader: React.FC<RecommendHeaderProps> = ({ children }) => {
   return (
-    <section className="flex flex-col gap-20">
-      <div className="flex flex-col items-center">
-        <BusinessSchedule />
-        <h2 className="mb-10 text-center text-3xl font-semibold">
-          <span className="text-balance">
-            Explora nuestros productos recomendados
-          </span>
-          <p className="text-lg text-secondary">
-            Nuestras opciones variadas de chocolates, cremas, lipbalm, entre
-            otros.
-          </p>
-        </h2>
+    <section className="relative flex w-full flex-col bg-primary-foreground p-40">
+      <div className="container mx-auto flex flex-col gap-40">
+        <div className="flex flex-col items-center">
+          <BusinessSchedule />
+          <h2 className="mb-10 text-center text-3xl font-semibold">
+            <span className="text-balance">
+              Explora nuestros productos recomendados
+            </span>
+            <p className="text-lg text-secondary">
+              Nuestras opciones variadas de chocolates, cremas, lipbalm, entre
+              otros.
+            </p>
+          </h2>
+        </div>
+        <div className="flex gap-6">{children}</div>
       </div>
-      <div className="flex gap-6">{children}</div>
+      <SeparateSection className="absolute bottom-full left-0 rotate-180 text-primary-foreground" />
+      <SeparateSection className="absolute left-0 top-full text-primary-foreground" />
     </section>
   );
 };
