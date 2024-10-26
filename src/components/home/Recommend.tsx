43293dcb08a032a8edebe71a28aadc2178c981f4
EleoRomario <eleo.romario@gmail.com>
2024-10-26 10:36:11 -0500

    refactor: actualizar espaciado y agregar componente LineTitle en la sección de talleres

--- src/components/home/Recommend.tsx
+++ src/components/home/Recommend.tsx
@@ -12,10 +12,10 @@ interface RecommendHeaderProps {
 
 const RecommendHeader: React.FC<RecommendHeaderProps> = ({ children }) => {
   return (
-    <section className="relative flex w-full flex-col bg-primary-foreground p-40">
-      <div className="container mx-auto flex flex-col gap-40">
+    <section className="relative flex w-full flex-col bg-primary-foreground py-40">
+      <div className="container mx-auto flex w-full flex-col gap-40">
         <div className="grid grid-cols-1 items-center gap-6 sm:grid-cols-[1fr_auto_1fr]">
-          <LineTitle className="w-full text-primary" />
+          <LineTitle className="hidden text-primary sm:flex" />
           <h2 className="mb-10 text-center text-3xl font-semibold">
             <span className="text-balance">
               Explora nuestros productos recomendados
@@ -25,9 +25,9 @@ const RecommendHeader: React.FC<RecommendHeaderProps> = ({ children }) => {
               otros.
             </p>
           </h2>
-          <LineTitle className="w-full rotate-180 text-primary" />
+          <LineTitle className="hidden rotate-180 text-primary sm:flex" />
         </div>
-        <div className="grid gap-y-40 [grid-template-columns:_repeat(auto-fit,_minmax(300px,_1fr))]">
+        <div className="grid justify-items-center gap-y-40 [grid-template-columns:_repeat(auto-fit,_minmax(300px,_1fr))]">
           {children}
         </div>
       </div>
