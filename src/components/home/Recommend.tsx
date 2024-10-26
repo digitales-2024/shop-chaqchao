eae18c0b160ce54cdbf7374b078bf3265d351077
EleoRomario <eleo.romario@gmail.com>
2024-10-26 10:27:31 -0500

    feat: agregar componente LineTitle en la sección de recomendados

--- src/components/home/Recommend.tsx
+++ src/components/home/Recommend.tsx
@@ -3,7 +3,7 @@ import { useCatalog } from "@/hooks/use-catalog";
 
 import { CartItem } from "../cart/CartItem";
 import { CartSkeleton } from "../cart/CartSkeleton";
-import { BusinessSchedule } from "../common/Schedule";
+import { LineTitle } from "../common/LineTitle";
 import { SeparateSection } from "../common/SeparateSection";
 
 interface RecommendHeaderProps {
@@ -14,8 +14,8 @@ const RecommendHeader: React.FC<RecommendHeaderProps> = ({ children }) => {
   return (
     <section className="relative flex w-full flex-col bg-primary-foreground p-40">
       <div className="container mx-auto flex flex-col gap-40">
-        <div className="flex flex-col items-center">
-          <BusinessSchedule />
+        <div className="grid grid-cols-1 items-center gap-6 sm:grid-cols-[1fr_auto_1fr]">
+          <LineTitle className="w-full text-primary" />
           <h2 className="mb-10 text-center text-3xl font-semibold">
             <span className="text-balance">
               Explora nuestros productos recomendados
@@ -25,6 +25,7 @@ const RecommendHeader: React.FC<RecommendHeaderProps> = ({ children }) => {
               otros.
             </p>
           </h2>
+          <LineTitle className="w-full rotate-180 text-primary" />
         </div>
         <div className="grid gap-y-40 [grid-template-columns:_repeat(auto-fit,_minmax(300px,_1fr))]">
           {children}
