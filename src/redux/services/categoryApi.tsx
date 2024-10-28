8967dca974eebf9dc25bb4d10f216863f2cdc10f
EleoRomario <eleo.romario@gmail.com>
2024-10-28 12:23:40 -0500

    feat: agregar soporte para obtener productos de una categoría en el hook useCategory

--- src/redux/services/categoryApi.tsx
+++ src/redux/services/categoryApi.tsx
@@ -1,4 +1,4 @@
-import { Category } from "@/types";
+import { Category, Product } from "@/types";
 import { createApi } from "@reduxjs/toolkit/query/react";
 
 import baseQueryWithReauth from "./baseQuery";
@@ -12,11 +12,19 @@ export const categoryApi = createApi({
       query: () => "/catalog/categories",
       providesTags: ["Category"],
     }),
-    getCategory: build.query<Category, number>({
+    getCategory: build.query<Category, string>({
       query: (id) => `categories/${id}`,
+      providesTags: ["Category"],
+    }),
+    getCategoryProducts: build.query<Product[], { id: string }>({
+      query: ({ id }) => `/catalog/products-category/${id}`,
       providesTags: ["Category"],
     }),
   }),
 });
 
-export const { useGetCategoriesQuery, useGetCategoryQuery } = categoryApi;
+export const {
+  useGetCategoriesQuery,
+  useGetCategoryQuery,
+  useGetCategoryProductsQuery,
+} = categoryApi;
