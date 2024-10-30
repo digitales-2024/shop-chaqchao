9c06553d8c55ad23c13bb307c357e2d2634b2b71
EleoRomario <eleo.romario@gmail.com>
2024-10-30 11:27:50 -0500

    refactor: ajustar estilos para mejorar la presentación y la disposición de los elementos

--- src/components/home/Recommend.tsx
+++ src/components/home/Recommend.tsx
@@ -19,13 +19,13 @@ const RecommendHeader: React.FC<RecommendHeaderProps> = ({ children }) => {
       <div className="container mx-auto flex w-full flex-col gap-40">
         <div className="grid grid-cols-1 items-center gap-6 sm:grid-cols-[1fr_auto_1fr]">
           <LineTitle className="hidden text-primary sm:flex" />
-          <h2 className="mb-10 text-center text-3xl font-semibold">
+          <h2 className="text-center text-3xl font-semibold">
             <span className="text-balance">{t("title")}</span>
             <p className="text-lg text-secondary">{t("subtitle")}</p>
           </h2>
           <LineTitle className="hidden rotate-180 text-primary sm:flex" />
         </div>
-        <div className="grid justify-items-center gap-y-40 [grid-template-columns:_repeat(auto-fit,_minmax(300px,_1fr))]">
+        <div className="grid justify-items-center gap-6 [grid-template-columns:_repeat(auto-fit,_minmax(22rem,_1fr))]">
           {children}
         </div>
       </div>
