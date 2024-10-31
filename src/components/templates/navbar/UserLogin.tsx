bbb988862a9aa34d95352268773214699c4e279b
EleoRomario <eleo.romario@gmail.com>
2024-10-31 11:51:37 -0500

    feat: mejorar el menú de usuario con traducciones y ajustes de estilo

--- src/components/templates/navbar/UserLogin.tsx
+++ src/components/templates/navbar/UserLogin.tsx
@@ -1,9 +1,12 @@
 "use client";
+import { OrderUpdateContext } from "@/contexts/OrderUpdateContext";
 import { useLogout } from "@/hooks/use-logout";
 import { useProfile } from "@/hooks/use-profile";
 import { getFirstLetter } from "@/utils/getFirstLetter";
 import { CreditCard, LogOut, NotebookPen, User, UserRound } from "lucide-react";
+import { useTranslations } from "next-intl";
 import Link from "next/link";
+import { useContext } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -15,12 +18,11 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { useContext } from "react";
-import { OrderUpdateContext } from "@/contexts/OrderUpdateContext";
 
 export const UserLogin = () => {
+  const t = useTranslations("profile");
   const { signOut } = useLogout();
-  const { hasOrderUpdates } = useContext(OrderUpdateContext);
+  const hasOrderUpdates = useContext(OrderUpdateContext);
   const { clienteData, isLoading } = useProfile();
   if (isLoading || !clienteData) {
     return (
@@ -40,7 +42,7 @@ export const UserLogin = () => {
         <DropdownMenuTrigger asChild>
           <Button
             variant="ghost"
-            className="relative size-10 rounded-full bg-background text-lg capitalize ring-0 ring-offset-0 transition-all duration-300 hover:scale-105 focus:ring-0 focus:ring-offset-0 focus-visible:border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+            className="relative size-10 rounded-full border-primary bg-background font-nunito text-lg font-black capitalize outline outline-primary ring-0 ring-offset-0 transition-all duration-300 hover:scale-105 focus:ring-0 focus:ring-offset-0 focus-visible:border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
           >
             {hasOrderUpdates && (
               <span className="absolute right-0 top-0 size-2 rounded-full bg-emerald-500" />
@@ -49,29 +51,42 @@ export const UserLogin = () => {
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56" align="end">
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuLabel className="flex flex-col gap-1">
+            <span className="truncate font-nunito text-lg font-bold capitalize">
+              {clienteData.name}
+            </span>
+            <span className="font-nunito text-xs text-slate-500">
+              {clienteData.email}
+            </span>
+          </DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuGroup>
-            <DropdownMenuItem>
-              <User className="mr-2 h-4 w-4" />
-              <Link href="/profile">Perfil</Link>
+            <DropdownMenuItem asChild>
+              <Link href="/profile" className="cursor-pointer">
+                <User className="mr-2 h-4 w-4" />
+                {t("profile")}
+              </Link>
             </DropdownMenuItem>
-            <DropdownMenuItem className="relative">
-              <CreditCard className="mr-2 h-4 w-4" />
-              <Link href="/orders">Pedidos</Link>
-              {hasOrderUpdates && (
-                <span className="absolute right-3 top-3 size-2 items-center rounded-full bg-emerald-500" />
-              )}
+            <DropdownMenuItem asChild>
+              <Link href="/orders" className="relative cursor-pointer">
+                <CreditCard className="mr-2 h-4 w-4" />
+                {t("orders")}
+                {hasOrderUpdates && (
+                  <span className="absolute right-3 top-3 size-2 items-center rounded-full bg-emerald-500" />
+                )}
+              </Link>
             </DropdownMenuItem>
-            <DropdownMenuItem>
-              <NotebookPen className="mr-2 h-4 w-4" />
-              <span>Reservas</span>
+            <DropdownMenuItem asChild>
+              <Link href="/classes" className="cursor-pointer">
+                <NotebookPen className="mr-2 h-4 w-4" />
+                <span>{t("class")}</span>
+              </Link>
             </DropdownMenuItem>
           </DropdownMenuGroup>
           <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={signOut}>
+          <DropdownMenuItem onClick={signOut} className="cursor-pointer">
             <LogOut className="mr-2 h-4 w-4" />
-            <span>Cerrar sesión</span>
+            <span>{t("signout")}</span>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
