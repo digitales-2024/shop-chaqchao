1543730da6b7ef395f92d043372617a128083c82
EleoRomario <eleo.romario@gmail.com>
2024-11-02 13:00:30 -0500

    feat: refactor useOrders hook to utilize useGetOrdersQuery and simplify order management

--- src/hooks/use-orders.ts
+++ src/hooks/use-orders.ts
@@ -1,71 +1,32 @@
-// src/hooks/use-orders.ts
-
-import {
-  useGetOrderClientByIdQuery,
-  useDownloadOrderPdfMutation,
-  useUpdateOrderStatusMutation,
-} from "@/redux/services/orderApi";
-import { toast } from "sonner";
-
+import { useGetOrdersQuery } from "@/redux/services/orderApi";
 import { socket } from "@/socket/socket";
 import { useEffect } from "react";
-
-type StatusTranslations = {
-  [key: string]: string;
-};
-
-const OrderStatusEs = {
-  PENDING: "Pendiente",
-  CONFIRMED: "Confirmado",
-  READY: "Listo",
-  COMPLETED: "Completado",
-  CANCELLED: "Cancelado",
-};
 
 export const useOrders = (clientId?: string) => {
   const {
     data: orders,
-    error,
     isLoading,
-    refetch: refetchOrders,
-  } = useGetOrderClientByIdQuery(
-    { id: clientId as string },
-    { skip: !clientId },
-  );
-
-  const [onDownload, { isLoading: isLoadingPdf, error: errorPdf }] =
-    useDownloadOrderPdfMutation();
-
-  const onDownloadPdf = async (id: string, display = "inline") => {
-    try {
-      const response = await onDownload({ id, display }).unwrap();
-      return response; // This should be a Blob representing the PDF
-    } catch (error) {
-      toast.error("Error al descargar el PDF del pedido");
-      throw error;
-    }
-  };
-
-  const [
-    updateOrderStatus,
-    { isLoading: isLoadingUpdateOrderStatus, error: errorUpdateOrderStatus },
-  ] = useUpdateOrderStatusMutation();
+    refetch,
+  } = useGetOrdersQuery({
+    refetchOnMountOrArgChange: true,
+    refetchOnReconnect: true,
+  });
 
   // Manejo de eventos del socket
   useEffect(() => {
     const handleNewOrder = () => {
       // Si ya tenemos data cargada, permitimos el refetch
       if (orders) {
-        refetchOrders();
+        refetch();
       }
     };
 
     const handleOrderStatusUpdated = () => {
       // Si ya tenemos data cargada, permitimos el refetch
       if (orders) {
-        refetchOrders();
+        refetch();
         if (clientId) {
-          refetchOrders();
+          refetch();
         }
       }
     };
@@ -78,55 +39,11 @@ export const useOrders = (clientId?: string) => {
       socket.off("new-order", handleNewOrder);
       socket.off("order-status-updated", handleOrderStatusUpdated);
     };
-  }, [orders, refetchOrders, clientId]);
-
-  const onOrderStatusUpdate = (id: string, status: string) => {
-    const promise = () =>
-      new Promise(async (resolve, reject) => {
-        try {
-          const result = await updateOrderStatus({ id, status });
-          if (
-            result.error &&
-            typeof result.error === "object" &&
-            "data" in result.error
-          ) {
-            const error = result.error.data.message;
-            reject(new Error(error));
-          }
-          if (result.error) {
-            reject(
-              new Error(
-                "Ocurrió un error inesperado, por favor intenta de nuevo",
-              ),
-            );
-          }
-          resolve(result);
-        } catch (error) {
-          reject(error);
-        }
-      });
-
-    function translateStatus(status: string) {
-      const translations: StatusTranslations = OrderStatusEs;
-      return translations[status] || status;
-    }
-    return toast.promise(promise(), {
-      loading: "Actualizando estado del pedido...",
-      success: `Estado de pedido a ${translateStatus(status).toUpperCase()} actualizado`,
-      error: (err) => err.message,
-    });
-  };
+  }, [orders, refetch, clientId]);
 
   return {
     orders,
-    error,
     isLoading,
-    onDownloadPdf,
-    isLoadingPdf,
-    errorPdf,
-    refetchOrders,
-    onOrderStatusUpdate,
-    isLoadingUpdateOrderStatus,
-    errorUpdateOrderStatus,
+    refetch,
   };
 };
