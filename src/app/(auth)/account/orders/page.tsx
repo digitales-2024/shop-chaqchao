f92a83e2c29150d6e87b282a4d28af58bd9245a0
EleoRomario <eleo.romario@gmail.com>
2024-11-04 10:37:38 -0500

    feat: cargar el diseño predeterminado de pedidos desde las cookies en PageOrders

--- src/app/(auth)/account/orders/page.tsx
+++ src/app/(auth)/account/orders/page.tsx
@@ -1,5 +1,10 @@
+import { cookies } from "next/headers";
+
 import { OrderClient } from "@/components/orders/OrderClient";
 
 export default function PageOrders() {
-  return <OrderClient defaultLayout={[]} />;
+  const layout = cookies().get("react-resizable-panels:layout:orders");
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+
+  return <OrderClient defaultLayout={defaultLayout} />;
 }
