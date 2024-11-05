f60d6cfeb857ae2360afc37d913be2a703838dfb
EleoRomario <eleo.romario@gmail.com>
2024-11-05 09:52:29 -0500

    feat: mejorar el diseño del componente CartSkeleton para una mejor presentación

--- src/components/cart/CartSkeleton.tsx
+++ src/components/cart/CartSkeleton.tsx
@@ -3,12 +3,14 @@ import { Skeleton } from "../ui/skeleton";
 
 export const CartSkeleton = () => {
   return (
-    <Card className="h-auto w-60 border-secondary/10 bg-transparent">
-      <div className="flex flex-col items-center justify-center gap-4 px-2 py-10">
-        <Skeleton className="size-32 rounded"></Skeleton>
-        <Skeleton className="h-20 w-48 rounded"></Skeleton>
+    <Card className="h-[40rem] w-[22rem] overflow-hidden border-secondary/10 bg-transparent">
+      <div className="group/product bg-transparento grid h-full w-full grid-rows-[1fr_200px] gap-10 p-4">
+        <Skeleton className="size-full rounded"></Skeleton>
+        <div className="flex flex-col gap-4">
+          <Skeleton className="h-20 w-full rounded"></Skeleton>
+          <Skeleton className="h-10 w-full rounded"></Skeleton>
+        </div>
       </div>
-      <Skeleton className="m-4 h-10"></Skeleton>
     </Card>
   );
 };
