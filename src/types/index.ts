09be4dc547b5a828530bc1d55f81fa4d0fbfbd21
EleoRomario <eleo.romario@gmail.com>
2024-11-05 09:43:28 -0500

    feat: agregar tipos de negocio y horarios de atención

--- src/types/index.ts
+++ src/types/index.ts
@@ -14,3 +14,4 @@ export type { ResetPassword } from "./resetPassword";
 export type { CustomErrorData, ErrorFormData } from "./error";
 export type { Category, Product } from "./catalog";
 export type { ClassClient } from "./classes";
+export type { Business } from "./business";
