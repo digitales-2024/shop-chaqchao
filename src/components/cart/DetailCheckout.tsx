02fc1807d93305b03ba6c38f41e665de3d335597
EleoRomario <eleo.romario@gmail.com>
2024-11-08 15:27:02 -0500

    feat: agregar componente DetailCheckout para mostrar detalles del pago y resumen del carrito

---
+++ src/components/cart/DetailCheckout.tsx
@@ -0,0 +1,78 @@
+"use client";
+import useCartDetail from "@/hooks/use-cart-detail";
+import { useBusiness } from "@/hooks/useBusiness";
+import useCartStore from "@/redux/store/cart";
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
+import { useTranslations } from "next-intl";
+
+import { Card, CardContent, CardHeader } from "../ui/card";
+import { Separator } from "../ui/separator";
+import { Skeleton } from "../ui/skeleton";
+import { TableCart } from "./TableCart";
+
+export const DetailCheckout = () => {
+  const { amountTotal } = useCartStore();
+  const { business, isLoading } = useBusiness();
+
+  const { fullDate, invoice } = useCartDetail();
+
+  const t = useTranslations("checkout.summary");
+  const i = useTranslations("checkout.invoice");
+
+  return (
+    <Card className="rounded-3xl border-slate-100 p-10 shadow-none">
+      <CardHeader>
+        <h2 className="text-3xl font-bold">{t("title")}</h2>
+      </CardHeader>
+
+      <CardContent className="flex flex-col gap-10">
+        <Separator />
+        <div className="grid grid-cols-2">
+          <div className="space-y-6">
+            <div className="flex flex-col gap-2">
+              <span className="text-gray-400">{t("address")}:</span>
+              <span className="font-bold">
+                {isLoading ? (
+                  <Skeleton className="h-8 w-32" />
+                ) : (
+                  business.businessInfo.address
+                )}
+              </span>
+            </div>
+            <div className="flex flex-col gap-2">
+              <span className="text-gray-400">{t("date")}:</span>
+              <span className="font-bold">
+                {fullDate ? (
+                  format(fullDate, "PPPp", { locale: es })
+                ) : (
+                  <Skeleton className="h-8 w-32" />
+                )}
+              </span>
+            </div>
+          </div>
+          <div className="space-y-6">
+            <div className="flex flex-col gap-2">
+              <span className="text-gray-400">{t("typeInvoice")}:</span>
+              <span className="font-bold">
+                {invoice ? i(invoice) : <Skeleton className="h-8 w-32" />}
+              </span>
+            </div>
+            <div className="flex flex-col gap-2">
+              <span className="text-gray-400">Recoger:</span>
+              <span className="font-bold">
+                <Skeleton className="h-8 w-32" />
+              </span>
+            </div>
+          </div>
+        </div>
+        <Separator />
+        <TableCart />
+        <p className="inline-flex justify-between text-xl font-bold">
+          {t("total")}
+          <span>S/. {amountTotal.toFixed(2)}</span>
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
