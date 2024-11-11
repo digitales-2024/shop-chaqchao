b77b796c33bf0b2e76862f74a69d6f0a627321eb
EleoRomario <eleo.romario@gmail.com>
2024-11-11 12:30:59 -0500

    feat: agregar ícono de eliminación de compras

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,4 @@ export { Facebook } from "./Facebook";
 export { Instagram } from "./Instagram";
 export { TripAdvisor } from "./TripAdvisor";
 export { GoogleIcon } from "./GoogleIcon";
+export { ShoppingDelete } from "./shopping/ShoppingDelete";
