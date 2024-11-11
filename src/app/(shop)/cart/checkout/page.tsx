88ad4ac967a3c6059415b9a859816ffeb1ce1368
EleoRomario <eleo.romario@gmail.com>
2024-11-11 12:53:22 -0500

    feat: renombrar página del carrito a checkout y agregar componente CartTitle

--- src/app/(shop)/cart/checkout/page.tsx
+++ src/app/(shop)/cart/checkout/page.tsx
@@ -1,10 +1,11 @@
+import { CartTitle } from "@/components/cart/CartTitle";
 import { Checkout } from "@/components/cart/Checkout";
 
 export default function PageCart() {
   return (
     <section className="container mx-auto flex flex-col gap-10 py-10">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-center text-5xl font-bold">Cart</h1>
+        <CartTitle />
       </div>
       <Checkout />
     </section>
