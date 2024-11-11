f899762957c741918c3e9feda634e224c63f42f5
EleoRomario <eleo.romario@gmail.com>
2024-11-11 12:49:17 -0500

    feat: agregar ícono de verificación de compras

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,3 +3,4 @@ export { Instagram } from "./Instagram";
 export { TripAdvisor } from "./TripAdvisor";
 export { GoogleIcon } from "./GoogleIcon";
 export { ShoppingDelete } from "./shopping/ShoppingDelete";
+export { ShoppingCheck } from "./shopping/ShoppingCheck";
