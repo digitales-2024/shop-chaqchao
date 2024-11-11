53b6d99df1f5aa2af744b4c5326e9d3926fef5ae
EleoRomario <eleo.romario@gmail.com>
2024-11-11 16:02:13 -0500

    feat: mejorar el componente StepEmail con soporte de perfil y mensajes de alerta

--- src/components/cart/StepEmail.tsx
+++ src/components/cart/StepEmail.tsx
@@ -1,14 +1,25 @@
 import useCartDetail from "@/hooks/use-cart-detail";
 import { useClients } from "@/hooks/use-client";
+import { useProfile } from "@/hooks/use-profile";
+import { getFirstLetter } from "@/utils/getFirstLetter";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Mail, RefreshCcw } from "lucide-react";
+import { AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
+import { Check, Mail, RefreshCcw, UserSearch } from "lucide-react";
+import { useTranslations } from "next-intl";
 import { useForm } from "react-hook-form";
-import { toast } from "sonner";
 import { z } from "zod";
 
-import { FormLogin } from "../account/login/FormLogin";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import PulsatingDots from "../common/PulsatingDots";
 import { Button } from "../ui/button";
-import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
 import {
   Form,
   FormControl,
@@ -19,9 +30,15 @@ import {
 } from "../ui/form";
 import { Input } from "../ui/input";
 import { Separator } from "../ui/separator";
+import { DialogLogin } from "./DialogLogin";
 
 export const StepEmail = () => {
-  const { login } = useCartDetail();
+  const { clientData, isLoading } = useProfile();
+
+  const t = useTranslations("checkout.login");
+  const c = useTranslations("checkout");
+
+  const { login, setLogin, handleStepComplete } = useCartDetail();
   const emailSchema = z.object({
     email: z.string().email("Ingrese un correo electrónico válido"),
   });
@@ -33,78 +50,121 @@ export const StepEmail = () => {
     defaultValues: login,
   });
 
-  const { findClient, isLoadingClientByEmail } = useClients();
+  const { findClient, isLoadingClientByEmail, dataClientByEmail } =
+    useClients();
 
   const onSubmit = async (data: { email: string }) => {
-    const existClient = await findClient({ email: data.email });
-    if (!existClient) {
-      toast.success("Cliente encontrado");
+    const response = await findClient({ email: data.email });
+
+    if (response.error) {
+      setLogin({ email: data.email, name: "" });
+      handleStepComplete(0);
     }
+  };
+
+  const handleContinue = () => {
+    setLogin({ email: clientData?.email ?? "", name: clientData?.name ?? "" });
+    handleStepComplete(0);
   };
 
   return (
     <div className="space-y-10">
       <div>
-        <h2 className="text-lg font-bold">¿Por qué necesitamos tu email?</h2>
-        <p className="text-sm">
-          Necesitamos tu email para enviarte la confirmación de tu pedido y
-          mantenernos en contacto contigo.
-        </p>
+        <h2 className="text-lg font-bold">{t("title")}</h2>
+        <p className="text-sm">{t("description")}</p>
       </div>
-      <div className="grid grid-cols-[1fr_auto_1fr] gap-4">
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="flex flex-col gap-2"
-          >
-            <FormField
-              name="email"
-              control={form.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel
-                    htmlFor="email"
-                    className="inline-flex items-center gap-2 text-sm font-bold"
-                  >
-                    <Mail className="size-4" />
-                    Email
-                  </FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button className="">
-              {isLoadingClientByEmail ? (
-                <>
-                  <RefreshCcw className="spin size-4" /> Buscando{" "}
-                </>
-              ) : (
-                "Continuar"
-              )}
-            </Button>
-          </form>
-        </Form>
-        <Separator orientation="vertical" />
-        <div className="flex flex-col justify-between">
-          <div>
-            <h3 className="text-lg font-bold">¿Ya tienes cuenta?</h3>
-            <p className="text-sm">
-              ¡Inicia sesión y sigue disfrutando de tus compras!
-            </p>
-          </div>
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button>Iniciar sesión</Button>
-            </DialogTrigger>
-            <DialogContent className="">
-              <FormLogin />
-            </DialogContent>
-          </Dialog>
+      {isLoading ? (
+        <PulsatingDots />
+      ) : clientData ? (
+        <div className="flex flex-col items-end justify-end gap-2">
+          <Card className="w-full">
+            <CardHeader>
+              <CardTitle className="inline-flex items-center gap-4">
+                <div className="relative flex size-10 items-center justify-center rounded-full border-primary bg-background font-nunito text-lg font-black capitalize outline outline-primary">
+                  {getFirstLetter(clientData.name)}
+                </div>
+                <div className="flex flex-col gap-2">
+                  <span className="capitalize">{clientData.name}</span>
+                  <CardDescription className="inline-flex items-center gap-4">
+                    <span className="truncate">{clientData.email}</span>
+                    <Check className="size-4 text-emerald-500" />
+                  </CardDescription>
+                </div>
+              </CardTitle>
+            </CardHeader>
+          </Card>
+          <Button type="button" onClick={handleContinue}>
+            {c("continue")}
+          </Button>
         </div>
-      </div>
+      ) : (
+        <div className="grid grid-cols-[1fr_auto_1fr] gap-4">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="flex flex-col gap-2"
+            >
+              <FormField
+                name="email"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel
+                      htmlFor="email"
+                      className="inline-flex items-center gap-2 text-sm font-bold"
+                    >
+                      <Mail className="size-4" />
+                      Email
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <AnimatePresence>
+                {dataClientByEmail ? (
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                  >
+                    <Alert className="border-cyan-500 text-cyan-500">
+                      <UserSearch className="size-4 stroke-cyan-500" />
+                      <AlertTitle className="font-bold">
+                        {t("alert.title")}
+                      </AlertTitle>
+                      <AlertDescription>
+                        {t("alert.description")}
+                      </AlertDescription>
+                    </Alert>
+                  </motion.div>
+                ) : null}
+              </AnimatePresence>
+              <Button variant="outline" className="border-primary text-primary">
+                {isLoadingClientByEmail ? (
+                  <>
+                    <RefreshCcw className="spin size-4" /> Buscando{" "}
+                  </>
+                ) : (
+                  "Continuar"
+                )}
+              </Button>
+            </form>
+          </Form>
+          <Separator orientation="vertical" />
+          <div className="flex flex-col justify-between">
+            <div>
+              <h3 className="text-lg font-bold">¿Ya tienes cuenta?</h3>
+              <p className="text-sm">
+                ¡Inicia sesión y sigue disfrutando de tus compras!
+              </p>
+            </div>
+            <DialogLogin />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
