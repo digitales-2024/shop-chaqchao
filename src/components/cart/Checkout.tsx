471717364814b09e81013e2a2400c4d62ed2f9d3
EleoRomario <eleo.romario@gmail.com>
2024-11-12 15:42:01 -0500

    feat: ajustar diseño del componente Checkout para mejorar la responsividad

--- src/components/cart/Checkout.tsx
+++ src/components/cart/Checkout.tsx
@@ -3,7 +3,7 @@ import { DetailCheckout } from "./DetailCheckout";
 
 export const Checkout = () => {
   return (
-    <div className="grid grid-cols-2 gap-8">
+    <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
       <CheckoutSteps />
       <DetailCheckout />
     </div>
