46cd26a5b497d8c9214aeb7dcfcb5769477fa64c
EleoRomario <eleo.romario@gmail.com>
2024-11-12 15:21:03 -0500

    feat: agregar validación de carrito y verificación de disponibilidad de ítems

--- src/hooks/use-cart.ts
+++ src/hooks/use-cart.ts
@@ -1,4 +1,6 @@
 import { useValidateCartMutation } from "@/redux/services/cartApi";
+import { CartItem } from "@/types";
+import { ErrorData } from "@/types/error";
 
 export const useCart = () => {
   const [
@@ -6,8 +8,35 @@ export const useCart = () => {
     { data: dataValidate, isLoading: isLoadingValidate, error: errorValidate },
   ] = useValidateCartMutation();
 
+  const validateCart = async (cartItems: CartItem[]) => {
+    try {
+      if (cartItems.length > 0) {
+        const response = await validate({
+          cartItems: cartItems.map((item) => item.id),
+        });
+
+        return response;
+      }
+    } catch (error) {}
+  };
+
+  const validateItem = (item: string) => {
+    if (errorValidate) {
+      const errorData = (
+        errorValidate as ErrorData<ErrorData<{ id: string }[]>>
+      ).data;
+      if (errorData) {
+        const isAvailable = errorData.data.some((error) => error.id === item);
+        return isAvailable;
+      }
+      return false;
+    }
+    return false;
+  };
+
   return {
-    validate,
+    validateCart,
+    validateItem,
     dataValidate,
     isLoadingValidate,
     errorValidate,
