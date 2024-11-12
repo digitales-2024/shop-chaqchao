c6f9cc71b20cffe386e956beba35ee78a93ec3f8
EleoRomario <eleo.romario@gmail.com>
2024-11-12 10:27:51 -0500

    feat: simplificar validación de número en InvoiceSchema y refactorizar tipos de factura

--- src/schemas/invoice.schema.ts
+++ src/schemas/invoice.schema.ts
@@ -1,24 +1,16 @@
-import useCartDetail from "@/hooks/use-cart-detail";
-import { INVOICES } from "@/types";
 import { useTranslations } from "next-intl";
 import { z } from "zod";
 
 export const InvoiceSchema = () => {
   const t = useTranslations("checkout.invoice.errors");
 
-  const { invoice } = useCartDetail();
   return z.object({
     documentType: z.string().min(1, {
       message: t("typeDoc"),
     }),
-    number: z
-      .string()
-      .min(invoice.typeInvoice === INVOICES[0] ? 8 : 11, {
-        message: invoice.typeInvoice === INVOICES[0] ? t("dni") : t("ruc"),
-      })
-      .regex(/^[0-9]+$/, {
-        message: t("number"),
-      }),
+    number: z.string().regex(/^[0-9]+$/, {
+      message: t("number"),
+    }),
 
     address: z.string().optional(),
     name: z.string().optional(),
--- src/types/invoice.ts
+++ src/types/invoice.ts
@@ -1,12 +1,29 @@
-export type InvoiceType = "RECEIPT" | "INVOICE";
+export enum InvoiceType {
+  RECEIPT = "RECEIPT",
+  INVOICE = "INVOICE",
+}
 
-export const INVOICES: InvoiceType[] = ["RECEIPT", "INVOICE"];
+export enum DocumentType {
+  PASSPORT = "PASSPORT",
+  DNI = "DNI",
+}
 
-export interface Invoice {
+export const INVOICES: InvoiceType[] = [
+  InvoiceType.RECEIPT,
+  InvoiceType.INVOICE,
+];
+export const DOCUMENT_TYPE: DocumentType[] = [
+  DocumentType.DNI,
+  DocumentType.PASSPORT,
+];
+
+export interface Receipt {
   documentType: string;
   number: string;
+}
+export interface Invoice extends Receipt {
   address?: string;
-  name?: string;
+  name: string;
 }
 
 export interface InvoiceData extends Invoice {
