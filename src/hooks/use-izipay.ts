c1b1c0a36e743969e40c1b9312e6c8d9812ae4c8
EleoRomario <eleo.romario@gmail.com>
2024-11-15 12:31:10 -0500

    feat: agregar hooks useIzipay y usePayment para manejo de pagos y carga de SDK

---
+++ src/hooks/use-izipay.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect } from "react";
+
+declare global {
+  interface Window {
+    Izipay: any;
+  }
+}
+
+const useIzipay = (token: string | null) => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://sandbox-checkout.izipay.pe/payments/v1/js/index.js";
+    script.async = true;
+    script.onload = () => {
+      console.log("Izipay SDK loaded");
+    };
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  const loadIzipayForm = (config: any, callback: (response: any) => void) => {
+    if (token && window.Izipay) {
+      try {
+        const checkout = new window.Izipay({ config });
+
+        checkout?.LoadForm({
+          authorization: token,
+          keyRSA: "RSA",
+          callbackResponse: callback,
+        });
+      } catch (error: any) {
+        console.error(error.message, error.Errors, error.date);
+      }
+    }
+  };
+
+  return { loadIzipayForm };
+};
+
+export default useIzipay;
