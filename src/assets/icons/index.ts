0f326731678899b6bbb1796ca2094ebc160cdc8a
EleoRomario <eleo.romario@gmail.com>
2024-11-15 12:18:42 -0500

    feat: agregar componente Izipay como ícono SVG y exportarlo

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -4,3 +4,4 @@ export { TripAdvisor } from "./TripAdvisor";
 export { GoogleIcon } from "./GoogleIcon";
 export { ShoppingDelete } from "./shopping/ShoppingDelete";
 export { ShoppingCheck } from "./shopping/ShoppingCheck";
+export { Izipay } from "./Izipay";
