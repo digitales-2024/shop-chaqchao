05fc83f488bbb71e3127d146ce3cd9e27c3fb8bc
EleoRomario <eleo.romario@gmail.com>
2024-11-15 15:48:26 -0500

    feat: agregar componente ButtonPayment para manejo de pagos

---
+++ src/components/cart/ButtonPayment.tsx
@@ -0,0 +1,32 @@
+import { Izipay } from "@/assets/icons";
+
+import { Button } from "../ui/button";
+
+interface ButtonPaymentProps {
+  onClick: () => void;
+  disabled: boolean;
+}
+
+export const ButtonPayment: React.FC<ButtonPaymentProps> = ({
+  onClick,
+  disabled,
+}) => {
+  return (
+    <Button
+      id="btnPayNow"
+      onClick={onClick}
+      disabled={disabled}
+      className="relative w-full overflow-hidden bg-izipay px-10 py-10 text-2xl font-bold hover:bg-izipay-foreground"
+    >
+      <div className="absolute -left-2 -top-2 rounded-md bg-white p-4 [clip-path:_polygon(0_0,_100%_0,_90%_70%,_0%_100%)]">
+        <Izipay
+          style={{
+            width: "3rem",
+            height: "auto",
+          }}
+        />
+      </div>
+      Realizar el pago
+    </Button>
+  );
+};
