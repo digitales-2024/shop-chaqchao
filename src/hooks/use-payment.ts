dfe7b1e0a74aefd4c08296f42de84b209e16719f
EleoRomario <eleo.romario@gmail.com>
2024-11-16 12:35:10 -0500

    feat: mejorar el manejo de errores en el hook usePayment y actualizar los mensajes de toast

--- src/hooks/use-payment.ts
+++ src/hooks/use-payment.ts
@@ -1,6 +1,7 @@
 import { ShoppingDelete } from "@/assets/icons";
 import { useGeneratePaymentTokenMutation } from "@/redux/services/paymentApi";
 import { CreatePayment } from "@/types";
+import { ShoppingBag } from "lucide-react";
 import { createElement } from "react";
 import { toast } from "sonner";
 
@@ -10,24 +11,25 @@ export const usePayment = () => {
   const [generatePaymentTokenMutation] = useGeneratePaymentTokenMutation();
 
   const generatePaymentToken = async (data: UsePayment) => {
-    console.log("🚀 ~ generatePaymentToken ~ data:", data);
     try {
       const response = await generatePaymentTokenMutation(data).unwrap();
 
       if (response.code !== "200") {
-        toast("Ops!", {
-          description: response.message,
-          icon: createElement(ShoppingDelete),
-          className: "text-rose-500",
+        toast("¡Bien hecho!", {
+          description: "Carrito validado correctamente.",
+          icon: createElement(ShoppingBag),
+          className: "text-emerald-500",
         });
       }
       return response.response.token;
     } catch (error) {
       toast("Ops!", {
-        description: "Algo salio mal, por favor intenta de nuevo.",
+        description: "Algo salio mal, por favor intenta de nuevo",
         icon: createElement(ShoppingDelete),
         className: "text-rose-500",
       });
+
+      return null;
     }
   };
 
