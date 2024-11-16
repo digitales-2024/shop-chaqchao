c5a8bd41f2ab890dcfd8b4bab5f6b70553164b90
EleoRomario <eleo.romario@gmail.com>
2024-11-16 12:34:30 -0500

    refactor: eliminar la propiedad 'disabled' del componente ButtonPayment

--- src/components/cart/ButtonPayment.tsx
+++ src/components/cart/ButtonPayment.tsx
@@ -4,18 +4,13 @@ import { Button } from "../ui/button";
 
 interface ButtonPaymentProps {
   onClick: () => void;
-  disabled: boolean;
 }
 
-export const ButtonPayment: React.FC<ButtonPaymentProps> = ({
-  onClick,
-  disabled,
-}) => {
+export const ButtonPayment: React.FC<ButtonPaymentProps> = ({ onClick }) => {
   return (
     <Button
       id="btnPayNow"
       onClick={onClick}
-      disabled={disabled}
       className="relative w-full overflow-hidden bg-izipay px-10 py-10 text-2xl font-bold hover:bg-izipay-foreground"
     >
       <div className="absolute -left-2 -top-2 rounded-md bg-white p-4 [clip-path:_polygon(0_0,_100%_0,_90%_70%,_0%_100%)]">
