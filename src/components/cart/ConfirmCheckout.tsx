1864687001cda0d8b6626dba5219b74ea1cd6c10
EleoRomario <eleo.romario@gmail.com>
2024-11-26 10:28:18 -0500

    feat: refactor ConfirmCheckout para integrar PaymentForm y eliminar PaymentConfirm

--- src/components/cart/ConfirmCheckout.tsx
+++ src/components/cart/ConfirmCheckout.tsx
@@ -1,12 +1,14 @@
 import { ShoppingCheck, ShoppingDelete } from "@/assets/icons";
+import { useCart } from "@/hooks/use-cart";
 import useCartDetail from "@/hooks/use-cart-detail";
 import { usePayment } from "@/hooks/use-payment";
 import useCartStore from "@/redux/store/cart";
-import { CartItem } from "@/types";
+import { CartItem, CartItemInfo, CreatePayment } from "@/types";
 import { getDataOrderDynamic } from "@/utils/getDataOrderDynamic";
 import { zodResolver } from "@hookform/resolvers/zod";
+import KRGlue from "@lyracom/embedded-form-glue";
 import { RefreshCcw } from "lucide-react";
-import { useTranslations } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
@@ -26,23 +28,17 @@ import { cn } from "@/lib/utils";
 
 import { Button, buttonVariants } from "../ui/button";
 import { Checkbox } from "../ui/checkbox";
-import PaymentConfirm from "./PaymentConfirm";
+import { AlertSuccessCheckout } from "./AlertSuccessCheckout";
+import PaymentForm from "./PaymentForm";
 
 const currency = "PEN";
 interface ConfirmCheckoutProps {
   validateCart: (cartItems: CartItem[]) => Promise<boolean>;
 }
 
-interface OrderInfo {
-  transactionId: string;
-  orderNumber: string;
-  merchantCode: string;
-  publicKey: string;
-  amount: string;
-  currency: string;
-}
-
 export const ConfirmCheckout = ({ validateCart }: ConfirmCheckoutProps) => {
+  const [openAlertDialog, setOpenAlertDialog] = useState<boolean>(false);
+
   const t = useTranslations("checkout.summary");
   const confirmCheckoutSchema = z.object({
     terms: z.boolean().refine(
@@ -71,21 +67,19 @@ export const ConfirmCheckout = ({ validateCart }: ConfirmCheckoutProps) => {
     },
   });
 
-  const { cartItems, amountTotal } = useCartStore();
+  const { cartItems, amountTotal, clearCart } = useCartStore();
+  const { checkoutCart, completeCart } = useCart();
+  const [token, setToken] = useState<string>("");
 
   const router = useRouter();
 
   const [showPayment, setShowPayment] = useState(false);
-  const [orderInfo, setOrderInfo] = useState<OrderInfo | null>(null);
-  const [token, setToken] = useState<string | null>(null);
+  const [orderInfo, setOrderInfo] = useState<CreatePayment | null>(null);
   const [isLoadingGenerateToken, setIsLoadingGenerateToken] = useState(false);
 
   const { generatePaymentToken } = usePayment();
 
   const onConfirm = async () => {
-    const merchantCode = process.env.NEXT_PUBLIC_IZIPAY_MERCHANT_CODE ?? "";
-    const publicKey = process.env.NEXT_PUBLIC_IZIPAY_PUBLIC_KEY ?? "";
-
     try {
       await validateCart(cartItems);
       if (cartItems.length === 0) {
@@ -100,42 +94,55 @@ export const ConfirmCheckout = ({ validateCart }: ConfirmCheckoutProps) => {
       setIsLoadingGenerateToken(true);
 
       // Generar un nuevo transactionId y orderNumber
-      const { transactionId, orderNumber } = getDataOrderDynamic();
+      const { orderNumber } = getDataOrderDynamic();
 
       // Calcular el monto total como string con dos decimales
-      const amount = amountTotal.toFixed(2).toString();
+      const amount = amountTotal;
 
       // Establecer el estado de orderInfo
-      const newOrderInfo: OrderInfo = {
-        transactionId,
-        orderNumber,
-        merchantCode,
-        publicKey,
-        amount,
+      const newOrderInfo: CreatePayment = {
+        amount: amount * 100,
         currency,
+        orderId: orderNumber,
+        customer: {
+          email: contact.email,
+          reference: contact.phone,
+          billingDetails: {
+            address: invoice.address,
+            city: invoice.city,
+            country: invoice.country,
+            state: invoice.state,
+            firstName: contact.name,
+            lastName: contact.lastName,
+            phoneNumber: contact.phone,
+            identityCode: invoice.number,
+          },
+          shoppingCart: {
+            cartItemInfo: cartItems.map(
+              (item) =>
+                ({
+                  productLabel: item.name,
+                  productType: "FOOD_AND_GROCERY",
+                  productQty: item.quantity,
+                  productAmount: item.price * 100,
+                }) as CartItemInfo,
+            ),
+          },
+        },
       };
 
       setOrderInfo(newOrderInfo);
-
       // Generar el token de pago
-      const tokenResponse = await generatePaymentToken({
-        transaction: transactionId,
-        body: {
-          requestSource: "ECOMMERCE",
-          merchantCode: merchantCode,
-          orderNumber: orderNumber,
-          publicKey: publicKey,
-          amount: amount,
-        },
-      });
-
+      const tokenResponse = await generatePaymentToken(newOrderInfo);
       if (!tokenResponse) {
         throw new Error("Error al generar token");
       }
 
-      setToken(tokenResponse ?? null);
       setIsLoadingGenerateToken(false);
-      setShowPayment(true);
+      // Completar el carrito
+      await completeCart();
+      setToken(tokenResponse.token ?? "");
+      payment(tokenResponse.token ?? "");
     } catch (error) {
       toast("Ops!", {
         description: "Algo salió mal, por favor intenta de nuevo.",
@@ -148,7 +155,9 @@ export const ConfirmCheckout = ({ validateCart }: ConfirmCheckoutProps) => {
     }
   };
 
-  const handlePaymentSuccess = () => {
+  const handlePaymentSuccess = async () => {
+    await checkoutCart();
+
     toast("¡Pago realizado con éxito!", {
       description: "Gracias por tu compra.",
       icon: <ShoppingCheck />,
@@ -156,9 +165,9 @@ export const ConfirmCheckout = ({ validateCart }: ConfirmCheckoutProps) => {
     });
     // Resetear estados o redireccionar según sea necesario
     setOrderInfo(null);
-    setToken(null);
     setShowPayment(false);
     // Opcional: Vaciar el carrito, redirigir, etc.
+    clearCart();
   };
 
   const handlePaymentError = () => {
@@ -169,7 +178,6 @@ export const ConfirmCheckout = ({ validateCart }: ConfirmCheckoutProps) => {
     });
     // Resetear orderInfo y token para permitir reintentar
     setOrderInfo(null);
-    setToken(null);
     setShowPayment(false);
   };
 
@@ -180,6 +188,67 @@ export const ConfirmCheckout = ({ validateCart }: ConfirmCheckoutProps) => {
     contact.email &&
     invoice.typeInvoice &&
     invoice.number;
+
+  const endpoint =
+    process.env.NEXT_PUBLIC_IZIPAY_PAYMENT_ENDPOINT ||
+    "https://api.micuentaweb.pe";
+  const publicKey = process.env.NEXT_PUBLIC_IZIPAY_PAYMENT_PUBLIC_KEY || "";
+
+  const locale = useLocale();
+
+  const payment = (token: string) => {
+    try {
+      getFormToken(token);
+      setShowPayment(true);
+    } catch (error) {
+      handlePaymentError();
+    }
+  };
+  const getFormToken = async (token: string) => {
+    try {
+      const { KR } = await KRGlue.loadLibrary(endpoint, publicKey);
+      await KR.setFormConfig({
+        formToken: token,
+        "kr-language": locale,
+        "kr-public-key": publicKey,
+      });
+
+      await KR.onError((error) => {
+        toast(error.errorMessage, {
+          description: error.detailedErrorMessage,
+          icon: <ShoppingDelete />,
+          className: "text-rose-500",
+        });
+      });
+
+      await KR.onPopinClosed(() => {
+        toast("¡Pago cancelado!", {
+          description: "El pago ha sido cancelado.",
+          icon: <ShoppingDelete />,
+          className: "text-rose-500",
+        });
+      });
+
+      await KR.onSubmit((paymentData) => {
+        if (paymentData.clientAnswer.orderStatus === "PAID") {
+          handlePaymentSuccess();
+        }
+        setOpenAlertDialog(true);
+        KR.closePopin("#formPayment"); // Close the popin after payment
+        return false;
+      });
+
+      await KR.renderElements(
+        "#formPayment",
+      ); /* Render the payment form into myPaymentForm div*/
+    } catch (error) {
+      toast("Ops!", {
+        description: "Algo salió mal, por favor intenta de nuevo.",
+        icon: <ShoppingDelete />,
+        className: "text-rose-500",
+      });
+    }
+  };
 
   return (
     <>
@@ -274,11 +343,25 @@ export const ConfirmCheckout = ({ validateCart }: ConfirmCheckoutProps) => {
         </form>
       </Form>
       {showPayment && orderInfo && (
-        <PaymentConfirm
-          token={token}
-          orderInfo={orderInfo}
-          onPaymentSuccess={handlePaymentSuccess}
-          onPaymentError={handlePaymentError}
+        <>
+          <div className="d-flex justify-content-center">
+            <div
+              id="myDIV"
+              className="formulario"
+              style={{ display: showPayment ? "block" : "none" }}
+            >
+              <div id="formPayment">
+                {/* Formulario de pago POPIN */}
+                <PaymentForm popin={true} token={token} />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+      {openAlertDialog && (
+        <AlertSuccessCheckout
+          isOpen={openAlertDialog}
+          setIsOpen={setOpenAlertDialog}
         />
       )}
     </>
