6b462bbfa6fb3b2cd546fd48689b4fad44880502
EleoRomario <eleo.romario@gmail.com>
2024-11-29 09:55:04 -0500

    feat: agregar selector de idioma en el componente LayoutShop

--- src/components/templates/LayoutShop.tsx
+++ src/components/templates/LayoutShop.tsx
@@ -1,6 +1,9 @@
+import { Locale } from "@/i18n/config";
+import { useLocale } from "next-intl";
 import { ReactNode } from "react";
 
 import { Footer } from "./footer/Footer";
+import { LanguageSelector } from "./navbar/LanguageSelector";
 import { Navbar } from "./navbar/Navbar";
 
 interface LayoutShopProps {
@@ -8,10 +11,13 @@ interface LayoutShopProps {
 }
 
 export function LayoutShop({ children }: LayoutShopProps) {
+  const locale = useLocale();
+
   return (
     <>
       <Navbar />
       <main>{children}</main>
+      <LanguageSelector defaultValue={locale as Locale} />
       <Footer />
     </>
   );
