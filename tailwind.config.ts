38701558e3f13794e1a7d0eed0b382d9ccd81ff5
EleoRomario <eleo.romario@gmail.com>
2024-11-29 12:09:17 -0500

    feat: actualizar colores y agregar animación de fondo en la configuración de Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,11 +14,6 @@ const config: Config = {
         nunito: ["var(--font-nunito-variable)"],
       },
       colors: {
-        "color-1": "hsl(var(--color-1))",
-        "color-2": "hsl(var(--color-2))",
-        "color-3": "hsl(var(--color-3))",
-        "color-4": "hsl(var(--color-4))",
-        "color-5": "hsl(var(--color-5))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         izipay: {
@@ -88,8 +83,14 @@ const config: Config = {
         "accordion-up": "accordion-up 0.2s ease-out",
         rainbow: "rainbow var(--speed, 2s) infinite linear",
         "gradient-animation": "gradient-animation 10s ease-in-out infinite",
+        "background-position-spin":
+          "background-position-spin 3000ms infinite alternate",
       },
       keyframes: {
+        "background-position-spin": {
+          "0%": { backgroundPosition: "top center" },
+          "100%": { backgroundPosition: "bottom center" },
+        },
         "gradient-animation": {
           "0%": { "background-position": "0% 50%" },
           "50%": { "background-position": "100% 50%" },
