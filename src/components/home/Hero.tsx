748f02bbc644396d71922fb4b433087ca514fcfc
EleoRomario <eleo.romario@gmail.com>
2024-11-29 12:08:45 -0500

    feat: mejorar el diseño del componente Hero con ajustes en el espaciado y la estructura del texto

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -41,7 +41,7 @@ export const Hero = () => {
   return (
     <section className="relative flex w-full flex-col items-start justify-center p-10">
       <div className="absolute bottom-0 right-0 top-0 h-full w-full bg-gradient-to-br" />
-      <div className="container z-10 mx-auto grid h-full w-full grid-cols-1 justify-center sm:grid-cols-2">
+      <div className="container z-10 mx-auto grid h-full w-full grid-cols-1 justify-center gap-10 sm:grid-cols-2">
         <motion.div
           initial="hidden"
           animate="show"
@@ -63,26 +63,28 @@ export const Hero = () => {
             <ChaqchaoName className="h-32 sm:h-56" />
           </motion.h1>
           <motion.p
-            className="mt-6 text-balance text-center md:text-2xl"
+            className="mt-6 flex flex-col text-balance text-center md:text-2xl"
             variants={FADE_DOWN_ANIMATION_VARIANTS}
           >
-            {t("textfirst")}{" "}
-            <span className="relative inline-flex text-primary">
-              <span>{t("textresalt")}</span>
-              <motion.svg
-                fill="none"
-                viewBox="0 0 645 25"
-                className="absolute -bottom-[6px] left-0 w-full stroke-2"
-              >
-                <motion.path
-                  initial={{ pathLength: 0 }}
-                  animate={{ pathLength: 1 }}
-                  transition={{ duration: 0.5, delay: 0.5 }}
-                  strokeDasharray="0 1"
-                  stroke="currentColor"
-                  d="M.5 16C127.5-1 239.4-.67 358 3c108.5 3.36 191.5 9.5 286.5 21.5"
-                />
-              </motion.svg>
+            <span className="inline-flex gap-2">
+              {t("textfirst")}{" "}
+              <span className="relative inline-flex text-primary">
+                {t("textresalt")}
+                <motion.svg
+                  fill="none"
+                  viewBox="0 0 645 25"
+                  className="absolute -bottom-[6px] left-0 w-full stroke-2"
+                >
+                  <motion.path
+                    initial={{ pathLength: 0 }}
+                    animate={{ pathLength: 1 }}
+                    transition={{ duration: 0.5, delay: 0.5 }}
+                    strokeDasharray="0 1"
+                    stroke="currentColor"
+                    d="M.5 16C127.5-1 239.4-.67 358 3c108.5 3.36 191.5 9.5 286.5 21.5"
+                  />
+                </motion.svg>
+              </span>
             </span>
             {t("textsecond")}
           </motion.p>
