00a14e93fbfdd65f73bbcc2ab708c7bafd0f7289
EleoRomario <eleo.romario@gmail.com>
2024-12-03 16:00:52 -0500

    feat: refactor Navbar para usar useWindowScrollPosition
    Mejorar la gestión del fondo al hacer scroll

--- src/components/templates/navbar/Navbar.tsx
+++ src/components/templates/navbar/Navbar.tsx
@@ -1,49 +1,30 @@
 "use client";
 import { ChaqchaoCharacter } from "@/assets/images/ChaqchaoCharacter";
-import { useMediaQuery } from "@/hooks/use-media-query";
+import { useWindowScrollPosition } from "@/hooks/use-window-scroll-position";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-
-import { CartSheet } from "@/components/cart/CartSheet";
 
 import { cn } from "@/lib/utils";
 
 import { MenuList } from "./MenuList";
 import { NavList } from "./NavList";
-import { SheetMenuMobil } from "./SheetMenuMobil";
-import { UserLogin } from "./UserLogin";
 
 export function Navbar() {
-  const [navBackground, setNavBackground] = useState(false);
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 1) {
-        setNavBackground(true);
-      } else {
-        setNavBackground(false);
-      }
-    };
+  const { y } = useWindowScrollPosition();
 
-    window.addEventListener("scroll", handleScroll);
+  const isScrolling = y > 1;
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const isDesktop = useMediaQuery("(max-width: 800px)");
-  if (!isDesktop) {
-    return (
+  return (
+    <header className="flex items-center justify-center">
       <nav
         className={cn(
-          "sticky top-6 z-50 mx-auto flex items-center rounded-full bg-[hsla(0,0%,93%,0.72)] px-6 backdrop-blur-lg transition-all duration-500",
+          "fixed top-6 z-50 mx-auto rounded-full bg-primary-foreground/50 px-6 backdrop-blur-md transition-all duration-500",
           {
-            "h-20 w-[70rem]": !navBackground,
-            "h-16 w-[50rem]": navBackground,
+            "h-20 w-[70rem]": !isScrolling,
+            "h-16 w-[50rem]": isScrolling,
           },
         )}
       >
-        <div className="container mx-auto grid w-full grid-cols-3 items-center justify-between">
+        <div className="grid w-full grid-cols-3 items-center justify-between">
           <div className="flex w-full">
             <Link
               href="/"
@@ -52,8 +33,8 @@ export function Navbar() {
             >
               <ChaqchaoCharacter
                 className={cn("transition-all duration-300", {
-                  "h-20": !navBackground,
-                  "h-14": navBackground,
+                  "h-20": !isScrolling,
+                  "h-14": isScrolling,
                 })}
               />
             </Link>
@@ -64,35 +45,6 @@ export function Navbar() {
           </div>
         </div>
       </nav>
-    );
-  }
-
-  return (
-    <nav
-      className={cn(
-        "sticky top-0 z-50 flex w-full items-center justify-between p-4 backdrop-blur-sm transition-all duration-300 lg:px-6",
-        {
-          "bg-white": navBackground,
-          "bg-primary-foreground": !navBackground,
-        },
-      )}
-    >
-      <div className="container mx-auto grid w-full grid-cols-[auto_1fr] items-center justify-center">
-        <div className="flex w-full">
-          <Link
-            href="/"
-            prefetch={true}
-            className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6"
-          >
-            <ChaqchaoCharacter className="h-10" />
-          </Link>
-        </div>
-        <div className="flex items-center justify-end gap-6">
-          <CartSheet />
-          <UserLogin />
-          <SheetMenuMobil />
-        </div>
-      </div>
-    </nav>
+    </header>
   );
 }
