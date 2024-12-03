80c40913fcaeec7fba46012d2e9399c544ce67c0
EleoRomario <eleo.romario@gmail.com>
2024-12-03 17:05:34 -0500

    feat: refactor NavigationButtons para usar componente Button y hacer onNext/onBack opcionales

--- src/components/ui/NavigationClassButtons.tsx
+++ src/components/ui/NavigationClassButtons.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import { Button } from "./button";
+
 interface NavigationButtonsProps {
   back: string;
   next: string;
-  onNext: () => void;
-  onBack: () => void;
+  onNext?: () => void;
+  onBack?: () => void;
 }
 
 export const NavigationButtons: React.FC<NavigationButtonsProps> = ({
@@ -14,17 +16,21 @@ export const NavigationButtons: React.FC<NavigationButtonsProps> = ({
   onBack,
 }) => (
   <>
-    <button
-      onClick={onBack}
-      className="w-36 rounded-full bg-primary px-0 py-3 text-base font-semibold text-white shadow-lg transition-all duration-300 ease-in-out hover:bg-secondary/90"
-    >
-      {back}
-    </button>
-    <button
-      onClick={onNext}
-      className="w-36 rounded-full bg-primary px-0 py-3 text-base font-semibold text-white shadow-lg transition-all duration-300 ease-in-out hover:bg-secondary/90"
-    >
-      {next}
-    </button>
+    {onBack && (
+      <Button
+        onClick={onBack}
+        className="w-36 rounded-full bg-primary px-0 py-3 text-base font-semibold"
+      >
+        {back}
+      </Button>
+    )}
+    {onNext && (
+      <Button
+        onClick={onNext}
+        className="w-36 rounded-full bg-primary px-0 py-3 text-base font-semibold"
+      >
+        {next}
+      </Button>
+    )}
   </>
 );
