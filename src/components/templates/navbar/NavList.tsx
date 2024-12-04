85388fad3501f805861a680c5b4d51f0c512625c
EleoRomario <eleo.romario@gmail.com>
2024-12-04 17:26:45 -0500

    feat: integrar hook useOpenMenu y ajustar estilos en NavList para mejorar la interactividad

--- src/components/templates/navbar/NavList.tsx
+++ src/components/templates/navbar/NavList.tsx
@@ -1,33 +1,41 @@
 "use client";
+import { useOpenMenu } from "@/hooks/use-open-menu";
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 import { CartSheet } from "@/components/cart/CartSheet";
 
+import { cn } from "@/lib/utils";
+
 import { SearchBar } from "./SearchBar";
 import { UserLogin } from "./UserLogin";
 
-const dataButtons = [
-  { label: "Search", component: <SearchBar /> },
-  { label: "Login", component: <UserLogin /> },
-  { label: "Cart", component: <CartSheet /> },
-];
 export function NavList() {
   const [elementFocused, setElementFocused] = useState<number | null>(null);
 
+  const dataButtons = [
+    { label: "Search", component: <SearchBar /> },
+    { label: "Login", component: <UserLogin /> },
+    { label: "Cart", component: <CartSheet /> },
+  ];
   const handleHoverButton = (index: number | null) => {
     setElementFocused(index);
   };
+
+  const { open } = useOpenMenu();
+
   return (
     <div
-      className="flex flex-row gap-2"
+      className={cn("flex flex-row gap-2", {
+        "flex-col-reverse": open,
+      })}
       onMouseLeave={() => {
         handleHoverButton(null);
       }}
     >
       {dataButtons.map((button, index) => (
         <div
-          className="relative inline-flex w-fit whitespace-nowrap rounded text-sm font-medium"
+          className="relative inline-flex aspect-square size-16 w-fit shrink-0 items-center justify-center whitespace-nowrap rounded-full"
           key={button.label}
           onMouseEnter={() => handleHoverButton(index)}
         >
