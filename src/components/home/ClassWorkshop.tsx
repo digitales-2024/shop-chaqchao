857824ab51d16f2bbf05f029f7eb500aea51e037
EleoRomario <eleo.romario@gmail.com>
2024-12-05 17:29:40 -0500

    feat: refactor ClassWorkshop para utilizar un array de imágenes y mejorar la presentación

--- src/components/home/ClassWorkshop.tsx
+++ src/components/home/ClassWorkshop.tsx
@@ -6,75 +6,42 @@ import Image from "next/image";
 import { InView } from "../core/InView";
 
 export const ClassWorkshop = () => {
+  const images = [
+    {
+      src: ChaqchaoWorkshop01.src,
+      alt: "Chaqchao Workshop 01",
+    },
+    {
+      src: ChaqchaoWorkshop02.src,
+      alt: "Chaqchao Workshop 02",
+    },
+    {
+      src: ChaqchaoWorkshop03.src,
+      alt: "Chaqchao Workshop 03",
+    },
+  ];
+
   return (
-    <div className="flex w-full flex-row items-center justify-end gap-20">
-      <div className="inline-flex items-end">
+    <div className="flex w-full flex-col items-center justify-end gap-20">
+      {images.map(({ src, alt }, idx) => (
         <InView
+          key={idx}
           variants={{
-            hidden: {
-              opacity: 0,
-              translateX: -50,
-            },
-            visible: {
-              opacity: 1,
-              translateX: 0,
-            },
+            hidden: { filter: "blur(10px)", opacity: 0 },
+            visible: { filter: "blur(0px)", opacity: 1 },
           }}
-          transition={{ duration: 0.5, ease: "easeInOut" }}
-          viewOptions={{ margin: "0px 0px -350px 0px" }}
-          className="overflow-hidden"
+          viewOptions={{ margin: "0px 0px -200px 0px" }}
+          transition={{ duration: 1, ease: "easeInOut" }}
         >
           <Image
-            src={ChaqchaoWorkshop01}
-            alt="Chaqchao Workshop 01"
-            className="h-auto w-[30rem] rounded-3xl"
+            className="mb-4 size-full rounded-lg object-contain"
+            width={400}
+            height={400}
+            src={src}
+            alt={alt}
           />
         </InView>
-      </div>
-      <div className="flex flex-col gap-20">
-        <InView
-          variants={{
-            hidden: {
-              opacity: 0,
-              translateX: 50,
-            },
-            visible: {
-              opacity: 1,
-              translateX: 0,
-            },
-          }}
-          transition={{ duration: 0.5, ease: "easeInOut" }}
-          viewOptions={{ margin: "0px 0px -350px 0px" }}
-          className="overflow-hidden"
-        >
-          <Image
-            src={ChaqchaoWorkshop02}
-            alt="Chaqchao Workshop 01"
-            className="h-auto w-[30rem] rounded-3xl"
-          />
-        </InView>
-        <InView
-          variants={{
-            hidden: {
-              opacity: 0,
-              translateX: 50,
-            },
-            visible: {
-              opacity: 1,
-              translateX: 0,
-            },
-          }}
-          transition={{ duration: 0.5, ease: "easeInOut" }}
-          viewOptions={{ margin: "0px 0px -350px 0px" }}
-          className="overflow-hidden"
-        >
-          <Image
-            src={ChaqchaoWorkshop03}
-            alt="Chaqchao Workshop 01"
-            className="h-auto w-[30rem] rounded-3xl"
-          />
-        </InView>
-      </div>
+      ))}
     </div>
   );
 };
