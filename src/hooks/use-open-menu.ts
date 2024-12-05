ce6bd7791de982d2644e33252a5bcea01b9755f4
EleoRomario <eleo.romario@gmail.com>
2024-12-04 23:19:35 -0500

    feat: agregar función onClose al hook useOpenMenu para gestionar el cierre del menú

--- src/hooks/use-open-menu.ts
+++ src/hooks/use-open-menu.ts
@@ -3,8 +3,10 @@ import { create } from "zustand";
 interface OpenMenuState {
   open: boolean;
   onOpenChange: () => void;
+  onClose: () => void;
 }
 export const useOpenMenu = create<OpenMenuState>((set) => ({
   open: false,
   onOpenChange: () => set((state) => ({ open: !state.open })),
+  onClose: () => set({ open: false }),
 }));
