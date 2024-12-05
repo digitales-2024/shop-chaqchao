9c45bc474580c4225431284a42a47383c163e152
EleoRomario <eleo.romario@gmail.com>
2024-12-04 23:18:04 -0500

    feat: mejorar la navegación con animaciones y ajustes en el estado del menú

--- src/components/templates/navbar/Navbar.tsx
+++ src/components/templates/navbar/Navbar.tsx
@@ -1,12 +1,11 @@
 "use client";
 import { ChaqchaoCharacter } from "@/assets/images/ChaqchaoCharacter";
-import useClickOutside from "@/hooks/use-click-outside";
 import { useOpenMenu } from "@/hooks/use-open-menu";
 import { useWindowScrollPosition } from "@/hooks/use-window-scroll-position";
 import { AnimatePresence, motion } from "framer-motion";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
 import { CartSheet } from "@/components/cart/CartSheet";
@@ -14,7 +13,6 @@ import { CartSheet } from "@/components/cart/CartSheet";
 import { cn } from "@/lib/utils";
 
 import { MenuToogle } from "./MenuToogle";
-import { SearchBar } from "./SearchBar";
 import { UserLogin } from "./UserLogin";
 
 export function Navbar() {
@@ -23,104 +21,65 @@ export function Navbar() {
   const isScrolling = y > 1;
 
   const { open, onOpenChange } = useOpenMenu();
+
   const variants = {
-    hidden: { filter: "blur(10px)", opacity: 0 },
-    visible: { filter: "blur(0px)", opacity: 1 },
+    open: { height: "100%" },
+    closed: { height: "96px" },
   };
 
   return (
     <header className="flex items-center justify-center">
-      <nav
+      <motion.nav
+        initial="closed"
+        animate={open ? "open" : "closed"}
+        variants={variants}
+        transition={{
+          duration: 0.3,
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+          visualDuration: 0.3,
+        }}
         className={cn(
-          "container fixed top-0 z-50 mx-auto px-6 transition-all duration-500 sm:top-3 sm:rounded-[3rem]",
+          "container fixed top-0 z-50 mx-auto flex items-center justify-center transition-all duration-500 sm:top-3 sm:rounded-[3rem]",
           {
-            "w-full sm:h-auto": !isScrolling,
-            "w-full bg-primary-foreground/80 backdrop-blur-md sm:h-auto sm:max-w-[60rem]":
+            "w-full": !isScrolling,
+            "w-full bg-primary-foreground/80 backdrop-blur-sm sm:max-w-[60rem]":
               isScrolling,
           },
           {
-            "bg-primary-foreground/80 pb-20 backdrop-blur-md": open,
+            "bg-primary-foreground/80 backdrop-blur-sm": open,
           },
         )}
       >
-        <AnimatePresence mode="wait">
-          {!open ? (
-            <div
-              className={cn(
-                "w-full grid-cols-2 items-center justify-between sm:grid",
-                {
-                  "p-0": !isScrolling,
-                  "p-4": isScrolling,
-                },
-              )}
-            >
-              <div className="flex w-full">
-                <Link
-                  href="/"
-                  prefetch={true}
-                  className="flex w-full items-center justify-center md:w-auto lg:mr-6"
-                >
-                  <ChaqchaoCharacter
-                    className={cn("transition-all duration-300", {
-                      "h-16 sm:h-32": !isScrolling,
-                      "h-10 sm:h-20": isScrolling,
-                    })}
-                  />
-                </Link>
-              </div>
-              <div className="flex w-full flex-row items-center justify-end">
-                <motion.div
-                  key="closed"
-                  initial="hidden"
-                  animate="visible"
-                  exit="hidden"
-                  variants={variants}
-                  transition={{ duration: 0.5 }}
-                  className="flex w-full flex-row items-center justify-end"
-                >
-                  <MenuList />
-                </motion.div>
-                <MenuToogle />
-              </div>
-            </div>
-          ) : (
-            <div
-              className={cn("flex flex-col justify-center", {
-                "p-0": !isScrolling,
-                "p-4": isScrolling,
-              })}
-            >
-              <div className="flex w-full items-center justify-between">
-                <Link
-                  href="/"
-                  prefetch={true}
-                  className="flex w-full items-center justify-center md:w-auto lg:mr-6"
-                >
-                  <ChaqchaoCharacter
-                    className={cn("transition-all duration-300", {
-                      "h-32": !isScrolling,
-                      "h-14": isScrolling,
-                    })}
-                  />
-                </Link>
-                <MenuToogle />
-              </div>
-              <motion.div
-                key="open"
-                initial="hidden"
-                animate="visible"
-                exit="hidden"
-                variants={variants}
-                transition={{ duration: 0.5 }}
-                className="flex w-full flex-col-reverse items-end justify-center"
-              >
-                <MenuList />
-              </motion.div>
-            </div>
+        <div
+          className={cn(
+            "flex h-full w-full flex-row items-start justify-between p-4",
+            {
+              "items-start": open,
+            },
           )}
-        </AnimatePresence>
-      </nav>
-      <MenuDialog open={open} onToggle={onOpenChange} />
+        >
+          <div className="flex w-fit">
+            <Link
+              href="/"
+              prefetch={true}
+              className="flex w-full items-center justify-center"
+            >
+              <ChaqchaoCharacter className="h-16 transition-all duration-300" />
+            </Link>
+          </div>
+          <div
+            className={cn("flex w-full flex-row items-center justify-end", {
+              "flex-col-reverse items-end": open,
+            })}
+          >
+            <MenuList />
+            <MenuToogle />
+          </div>
+        </div>
+      </motion.nav>
+      {open && <MenuDialog open={open} onToggle={onOpenChange} />}
     </header>
   );
 }
@@ -135,7 +94,6 @@ const MenuList = () => {
   }[] = [
     { type: "link", label: t("products"), href: "/categories" },
     { type: "link", label: t("classes"), href: "/workshops" },
-    { type: "component", label: "Search", component: <SearchBar /> },
     { type: "component", label: "Login", component: <UserLogin /> },
     { type: "component", label: "Cart", component: <CartSheet /> },
   ];
@@ -145,7 +103,9 @@ const MenuList = () => {
   const handleHoverLink = (index: number | null) => {
     setElementFocused(index);
   };
-  const { open } = useOpenMenu();
+
+  const { open, onClose } = useOpenMenu();
+
   return (
     <nav
       className={cn("flex flex-row items-center justify-center gap-x-2", {
@@ -160,14 +120,15 @@ const MenuList = () => {
           <Link
             href={link.href ?? "/"}
             className={cn(
-              "relative inline-flex w-auto justify-center whitespace-nowrap rounded p-4 text-center text-2xl font-normal lowercase transition-all duration-300 hover:font-bold",
+              "relative hidden w-auto justify-center whitespace-nowrap rounded p-4 text-center text-2xl font-normal lowercase transition-all duration-300 hover:font-bold sm:inline-flex",
               {
-                "text-end uppercase": open,
+                "inline-flex uppercase": open,
               },
             )}
             key={link.label}
             onMouseEnter={() => handleHoverLink(index)}
             type="button"
+            onClick={onClose}
           >
             {link.label}
             <AnimatePresence>
@@ -186,9 +147,16 @@ const MenuList = () => {
           </Link>
         ) : (
           <div
-            className="relative inline-flex aspect-square size-16 w-fit shrink-0 items-center justify-center whitespace-nowrap rounded-full"
+            className={cn(
+              "relative inline-flex w-fit items-center justify-center whitespace-nowrap rounded-full text-2xl transition-all duration-300 hover:font-bold",
+              {
+                uppercase: open,
+                "aspect-square size-16 shrink-0": !open,
+              },
+            )}
             key={link.label}
             onMouseEnter={() => handleHoverLink(index)}
+            onClick={onClose}
           >
             {link.component}
             <AnimatePresence>
@@ -218,7 +186,6 @@ const MenuDialog = ({
   open: boolean;
   onToggle: () => void;
 }) => {
-  const containerRef = useRef(null);
   useEffect(() => {
     if (open) {
       document.body.style.overflow = "hidden";
@@ -231,17 +198,11 @@ const MenuDialog = ({
     };
   }, [open]);
 
-  useClickOutside(containerRef, () => {
-    console.log("click outside");
-    if (open) {
-      onToggle();
-    }
-  });
   if (!open) return null; // Si no está abierto, no renderizamos nada
   return createPortal(
     <div
-      ref={containerRef}
       className="fixed inset-0 z-40 flex items-center justify-center bg-white/10 backdrop-blur-sm"
+      onClick={onToggle}
     ></div>,
     document.body,
   );
