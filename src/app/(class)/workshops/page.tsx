0ef7f692b0bf133b86d38a8df6c9ca22bb6bd76d
EleoRomario <eleo.romario@gmail.com>
2024-12-06 11:52:08 -0500

    feat: reemplazar componente Meteors con fondo de gradiente radial y hero en la página de reservas

--- src/app/(class)/workshops/page.tsx
+++ src/app/(class)/workshops/page.tsx
@@ -2,13 +2,13 @@
 
 import { ReservationSummary } from "@/components/classes/ReservationSumary";
 import { StepsClass } from "@/components/classes/Steps";
-import Meteors from "@/components/core/Meteors";
 
 export default function ReservationPage() {
   return (
     <section className="relative">
-      <Meteors number={20} />
-      <div className="container mx-auto grid min-h-screen grid-cols-2 items-center justify-center gap-4 py-32">
+      <div className="hero absolute bottom-0 right-0 top-0 z-[1] h-full w-full opacity-10" />
+      <div className="absolute bottom-0 right-0 top-0 z-[2] h-full w-full bg-gradient-radial from-transparent via-white to-white" />
+      <div className="container relative z-[3] mx-auto grid min-h-screen grid-cols-2 items-center justify-center gap-4 py-32">
         <ReservationSummary />
         <StepsClass />
       </div>
