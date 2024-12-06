c4f648da441e546f74f5b77e760806a6040fdda0
EleoRomario <eleo.romario@gmail.com>
2024-12-06 10:01:36 -0500

    feat: agregar imágenes de talleres y mejorar la interacción con el cursor

--- src/components/home/Workshops.tsx
+++ src/components/home/Workshops.tsx
@@ -1,11 +1,16 @@
 "use client";
+import ChaqchaoWorkshop01 from "@/assets/images/workshop_01.webp";
+import ChaqchaoWorkshop02 from "@/assets/images/workshop_02.webp";
 import { ChevronRight, Notebook } from "lucide-react";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 
 import { AnimatedGradientText } from "../common/AnimatedGradientText";
+import BlobCursor from "../core/BlobCursor";
+import { Cursor } from "../core/Cursor";
 import { InView } from "../core/InView";
 import { ClassWorkshop } from "./ClassWorkshop";
 
@@ -24,27 +29,75 @@ export const Workshops = () => {
 
   return (
     <>
-      {/* <BlobCursor /> */}
+      <BlobCursor />
       <section className="container mx-auto flex flex-col gap-16 py-20">
-        <div className="grid grid-cols-1 items-start justify-start gap-8 sm:grid-cols-2">
-          <div className="top-32 flex w-full flex-col items-start justify-center gap-6 space-y-4 sm:sticky">
+        <div className="grid h-[620px] grid-cols-1 items-start justify-start gap-8 sm:grid-cols-2">
+          <div className="flex w-full flex-col items-start justify-center gap-6 space-y-4">
             <h2 className="flex w-fit flex-col gap-2">
               <span className="relative inline-flex gap-2">
-                <span className="absolute -top-2 text-5xl font-bold">Del</span>
-                <span className="relative font-pacifico text-[8rem] font-bold">
+                <span className="absolute -top-2 text-5xl font-bold capitalize">
+                  {t("title.start")}
+                </span>
+                <span className="relative font-pacifico text-[8rem] font-bold capitalize">
+                  <Cursor
+                    attachToParent
+                    variants={{
+                      initial: { height: 0, opacity: 0, scale: 0.3 },
+                      animate: { height: "auto", opacity: 1, scale: 1 },
+                      exit: { height: 0, opacity: 0, scale: 0.3 },
+                    }}
+                    transition={{
+                      type: "spring",
+                      duration: 0.3,
+                      bounce: 0.1,
+                    }}
+                    className="overflow-hidden"
+                    springConfig={{
+                      bounce: 0.01,
+                    }}
+                  >
+                    <Image
+                      src={ChaqchaoWorkshop01}
+                      alt="Christian Church, Eastern Europe"
+                      className="h-40 w-auto rounded-xl"
+                    />
+                  </Cursor>
                   <InView
                     variants={variants1}
                     viewOptions={{ margin: "0px 0px -200px 0px" }}
                     transition={{ duration: 1, ease: "easeInOut" }}
                   >
-                    Cafe
+                    {t("title.resalt")}
                   </InView>
                 </span>
               </span>
               <span className="relative ml-20 inline-flex gap-2">
                 <span className="absolute right-1/2 text-5xl font-bold">
-                  al
+                  {t("title.end")}
                 </span>
+                <Cursor
+                  attachToParent
+                  variants={{
+                    initial: { height: 0, opacity: 0, scale: 0.3 },
+                    animate: { height: "auto", opacity: 1, scale: 1 },
+                    exit: { height: 0, opacity: 0, scale: 0.3 },
+                  }}
+                  transition={{
+                    type: "spring",
+                    duration: 0.3,
+                    bounce: 0.1,
+                  }}
+                  className="overflow-hidden"
+                  springConfig={{
+                    bounce: 0.01,
+                  }}
+                >
+                  <Image
+                    src={ChaqchaoWorkshop02}
+                    alt="Christian Church, Eastern Europe"
+                    className="h-40 w-auto rounded-xl"
+                  />
+                </Cursor>
                 <InView
                   variants={variants2}
                   viewOptions={{ margin: "0px 0px -200px 0px" }}
