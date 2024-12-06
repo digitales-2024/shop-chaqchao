43fbd8dd1c3710d26cf3e751bedcb3e097384236
EleoRomario <eleo.romario@gmail.com>
2024-12-06 15:11:04 -0500

    feat: agregar funcionalidad para descargar PDF de pedidos y mejorar la gestión de pedidos

--- src/hooks/use-orders.ts
+++ src/hooks/use-orders.ts
@@ -1,6 +1,11 @@
-import { useGetOrdersQuery } from "@/redux/services/orderApi";
+import {
+  useDownloadOrderPdfMutation,
+  useGetOrderQuery,
+  useGetOrdersQuery,
+} from "@/redux/services/orderApi";
 import { socket } from "@/socket/socket";
 import { useEffect } from "react";
+import { toast } from "sonner";
 
 export const useOrders = (clientId?: string) => {
   const {
@@ -11,6 +16,9 @@ export const useOrders = (clientId?: string) => {
     refetchOnMountOrArgChange: true,
     refetchOnReconnect: true,
   });
+
+  const [onDownload, { isLoading: isLoadingPdf, error: errorPdf }] =
+    useDownloadOrderPdfMutation();
 
   // Manejo de eventos del socket
   useEffect(() => {
@@ -41,9 +49,44 @@ export const useOrders = (clientId?: string) => {
     };
   }, [orders, refetch, clientId]);
 
+  const {
+    data: order,
+    isLoading: isLoadingOrder,
+    error: errorOrder,
+  } = useGetOrderQuery(clientId || "", {
+    skip: !clientId,
+  });
+
+  const onDownloadPdf = async (id: string, code: string) => {
+    try {
+      const response = await onDownload({ id }).unwrap();
+      // Crear el enlace de descarga
+      const url = window.URL.createObjectURL(response);
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", `pedido-${code}.pdf`);
+
+      // Añadir el enlace al DOM y disparar la descarga
+      document.body.appendChild(link);
+      link.click();
+
+      // Eliminar el enlace temporal del DOM
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url); // Limpiar el objeto URL
+    } catch (error) {
+      toast.error("Error al descargar el PDF del pedido");
+    }
+  };
+
   return {
     orders,
     isLoading,
     refetch,
+    order,
+    isLoadingOrder,
+    errorOrder,
+    onDownloadPdf,
+    isLoadingPdf,
+    errorPdf,
   };
 };
