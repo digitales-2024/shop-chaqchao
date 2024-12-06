314b6d536eab2432a9ccf4813ac0397ba6a2177d
EleoRomario <eleo.romario@gmail.com>
2024-12-06 10:57:04 -0500

    feat: agregar fondo decorativo y ajustar el contenedor en LayoutShop

--- src/components/templates/LayoutShop.tsx
+++ src/components/templates/LayoutShop.tsx
@@ -10,7 +10,8 @@ interface LayoutShopProps {
 
 export function LayoutShop({ children }: LayoutShopProps) {
   return (
-    <div className="overflow-hidden rounded-2xl bg-white">
+    <div className="relative overflow-hidden rounded-2xl bg-white">
+      <div className="pointer-events-none fixed inset-0 z-10 h-full w-full rounded-none opacity-[0.08] [background-image:url('https://framerusercontent.com/images/rR6HYXBrMmX4cRpXfXUOvpvpB0.png');] [background-repeat:repeat;] [background-size:_109px;]"></div>
       <Navbar />
       <main className="py-40">{children}</main>
       <LanguageSelector />
