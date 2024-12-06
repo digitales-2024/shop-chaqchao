863642670fe235eb534261823f1ec1b1ead9aed3
EleoRomario <eleo.romario@gmail.com>
2024-12-06 11:42:09 -0500

    feat: agregar nuevos gradientes de fondo radial y cónico en la configuración de Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -169,6 +169,11 @@ const config: Config = {
           },
         },
       },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
