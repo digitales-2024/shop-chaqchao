dca27ceefa51eff6b30918d3007b78a91fa24cf1
EleoRomario <eleo.romario@gmail.com>
2024-12-06 12:17:04 -0500

    feat: eliminar componente LayoutClass y ajustar estilos en las páginas de reclamaciones y talleres

--- src/app/(class)/workshops/page.tsx
+++ src/app/(class)/workshops/page.tsx
@@ -8,7 +8,7 @@ export default function ReservationPage() {
     <section className="relative">
       <div className="hero absolute bottom-0 right-0 top-0 z-[1] h-full w-full opacity-10" />
       <div className="absolute bottom-0 right-0 top-0 z-[2] h-full w-full bg-gradient-radial from-transparent via-white to-white" />
-      <div className="container relative z-[3] mx-auto grid min-h-screen grid-cols-2 items-center justify-center gap-4 py-32">
+      <div className="container relative z-[3] mx-auto flex flex-col-reverse items-center justify-center gap-4 sm:grid sm:grid-cols-2">
         <ReservationSummary />
         <StepsClass />
       </div>
