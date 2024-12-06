6e099e4d8d6cf0265023341d8af5c60dc1e7c844
EleoRomario <eleo.romario@gmail.com>
2024-12-06 10:56:55 -0500

    feat: mejorar el diseño y la animación del componente Workshops

--- src/components/home/Workshops.tsx
+++ src/components/home/Workshops.tsx
@@ -1,14 +1,11 @@
 "use client";
 import ChaqchaoWorkshop01 from "@/assets/images/workshop_01.webp";
 import ChaqchaoWorkshop02 from "@/assets/images/workshop_02.webp";
-import { ChevronRight, Notebook } from "lucide-react";
+import { motion } from "framer-motion";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
 
-import { cn } from "@/lib/utils";
-
-import { AnimatedGradientText } from "../common/AnimatedGradientText";
 import BlobCursor from "../core/BlobCursor";
 import { Cursor } from "../core/Cursor";
 import { InView } from "../core/InView";
@@ -28,53 +25,53 @@ export const Workshops = () => {
   };
 
   return (
-    <>
+    <section className="container relative mx-auto flex flex-col gap-16 py-20">
       <BlobCursor />
-      <section className="container mx-auto flex flex-col gap-16 py-20">
-        <div className="grid h-[620px] grid-cols-1 items-start justify-start gap-8 sm:grid-cols-2">
-          <div className="flex w-full flex-col items-start justify-center gap-6 space-y-4">
-            <h2 className="flex w-fit flex-col gap-2">
-              <span className="relative inline-flex gap-2">
-                <span className="absolute -top-2 text-5xl font-bold capitalize">
-                  {t("title.start")}
-                </span>
-                <span className="relative font-pacifico text-[8rem] font-bold capitalize">
-                  <Cursor
-                    attachToParent
-                    variants={{
-                      initial: { height: 0, opacity: 0, scale: 0.3 },
-                      animate: { height: "auto", opacity: 1, scale: 1 },
-                      exit: { height: 0, opacity: 0, scale: 0.3 },
-                    }}
-                    transition={{
-                      type: "spring",
-                      duration: 0.3,
-                      bounce: 0.1,
-                    }}
-                    className="overflow-hidden"
-                    springConfig={{
-                      bounce: 0.01,
-                    }}
-                  >
-                    <Image
-                      src={ChaqchaoWorkshop01}
-                      alt="Christian Church, Eastern Europe"
-                      className="h-40 w-auto rounded-xl"
-                    />
-                  </Cursor>
-                  <InView
-                    variants={variants1}
-                    viewOptions={{ margin: "0px 0px -200px 0px" }}
-                    transition={{ duration: 1, ease: "easeInOut" }}
-                  >
-                    {t("title.resalt")}
-                  </InView>
-                </span>
+      <div className="grid h-[620px] grid-cols-1 items-start justify-start gap-8 px-4 sm:grid-cols-2 sm:px-0">
+        <div className="flex w-full flex-col items-start justify-center gap-6 space-y-4">
+          <h2 className="flex w-fit flex-col gap-y-14">
+            <span className="relative inline-flex h-auto">
+              <span className="absolute -top-12 h-auto text-5xl font-bold capitalize">
+                {t("title.start")}
               </span>
-              <span className="relative ml-20 inline-flex gap-2">
-                <span className="absolute right-1/2 text-5xl font-bold">
-                  {t("title.end")}
-                </span>
+              <span className="relative h-auto font-pacifico text-[6rem] font-bold capitalize sm:text-[8rem]">
+                <Cursor
+                  attachToParent
+                  variants={{
+                    initial: { height: 0, opacity: 0, scale: 0.3 },
+                    animate: { height: "auto", opacity: 1, scale: 1 },
+                    exit: { height: 0, opacity: 0, scale: 0.3 },
+                  }}
+                  transition={{
+                    type: "spring",
+                    duration: 0.3,
+                    bounce: 0.1,
+                  }}
+                  className="overflow-hidden"
+                  springConfig={{
+                    bounce: 0.01,
+                  }}
+                >
+                  <Image
+                    src={ChaqchaoWorkshop01}
+                    alt="Christian Church, Eastern Europe"
+                    className="h-40 w-auto rounded-xl"
+                  />
+                </Cursor>
+                <InView
+                  variants={variants1}
+                  viewOptions={{ margin: "0px 0px -200px 0px" }}
+                  transition={{ duration: 1, ease: "easeInOut" }}
+                >
+                  {t("title.resalt")}
+                </InView>
+              </span>
+            </span>
+            <span className="relative ml-0 inline-flex h-auto gap-2 whitespace-normal sm:ml-20">
+              <span className="absolute right-1/2 top-0 text-5xl font-bold">
+                {t("title.end")}
+              </span>
+              <span className="font-pacifico text-[5rem] font-extrabold capitalize sm:text-[9rem]">
                 <Cursor
                   attachToParent
                   variants={{
@@ -103,31 +100,51 @@ export const Workshops = () => {
                   viewOptions={{ margin: "0px 0px -200px 0px" }}
                   transition={{ duration: 1, ease: "easeInOut" }}
                 >
-                  <span className="font-pacifico text-[9rem] font-extrabold capitalize">
-                    Chocolate
-                  </span>
+                  Chocolate
                 </InView>
               </span>
-            </h2>
-            <p className="text-2xl text-gray-500">{t("subtitle")}</p>
-            <Link href="/workshops">
-              <AnimatedGradientText>
-                <Notebook className="text-secondary" />{" "}
-                <hr className="mx-2 h-4 w-px shrink-0 bg-gray-300" />{" "}
-                <span
-                  className={cn(
-                    `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-secondary to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-xl text-transparent`,
-                  )}
-                >
-                  {t("button")}
-                </span>
-                <ChevronRight className="ml-1 size-5 text-secondary transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
-              </AnimatedGradientText>
-            </Link>
-          </div>
-          <ClassWorkshop />
+            </span>
+          </h2>
+          <p className="text-2xl text-gray-500">{t("subtitle")}</p>
+          <Link href="/workshops">
+            <motion.span
+              className="inline-flex overflow-hidden rounded-full bg-[linear-gradient(120deg,#FFf_calc(var(--shimmer-button-x)-25%),#ffb31c_var(--shimmer-button-x),#f38e1b_calc(var(--shimmer-button-x)+25%))] [--shimmer-button-x:0%]"
+              initial={
+                {
+                  scale: 1,
+                  "--shimmer-button-x": "-100%",
+                } as any
+              }
+              animate={
+                {
+                  "--shimmer-button-x": "200%",
+                } as any
+              }
+              transition={{
+                stiffness: 500,
+                damping: 20,
+                type: "spring",
+                "--shimmer-button-x": {
+                  duration: 3,
+                  repeat: Infinity,
+                  ease: [0.445, 0.05, 0.55, 0.95],
+                },
+              }}
+              whileTap={{
+                scale: 0.95,
+              }}
+              whileHover={{
+                scale: 1.05,
+              }}
+            >
+              <span className="m-[0.125rem] rounded-full bg-secondary px-8 py-4 text-2xl font-semibold backdrop-blur-sm">
+                {t("button")}
+              </span>
+            </motion.span>
+          </Link>
         </div>
-      </section>
-    </>
+        <ClassWorkshop />
+      </div>
+    </section>
   );
 };
