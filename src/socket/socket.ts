41b92a19d91b3851e371d1fb0cab42ffc1397a65
EleoRomario <eleo.romario@gmail.com>
2024-12-07 11:05:35 -0500

    feat: actualizar configuración de socket y utilizar variable de entorno para la URL

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,8 +1,11 @@
 import { Manager } from "socket.io-client";
 
-const manager = new Manager("http://localhost:5000", {
-  reconnectionAttempts: 3,
-  reconnectionDelay: 1000,
-});
+const manager = new Manager(
+  process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:5000",
+  {
+    reconnectionAttempts: 3,
+    reconnectionDelay: 1000,
+  },
+);
 
 export const socket = manager.socket("/");
