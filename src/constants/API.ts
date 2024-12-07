2883d2e08b650caa3e09cdb6ff1b0252b6b36f9f
EleoRomario <eleo.romario@gmail.com>
2024-12-07 10:41:51 -0500

    feat: actualizar configuración de API y eliminar constantes obsoletas

--- src/constants/API.ts
+++
@@ -1,1 +0,0 @@
-export const LINK_API_SUNAT = "https://dniruc.apisperu.com/api/v1";
--- src/lib/api/api-sunat.tsx
+++ src/lib/api/api-sunat.tsx
@@ -1,11 +1,9 @@
-import { LINK_API_SUNAT } from "@/constants/API";
-
 export async function getCustomerData(
   documentType: string,
   documentNumber: string,
 ) {
   const response = await fetch(
-    `${LINK_API_SUNAT}/${documentType}/${documentNumber}?token=${process.env.NEXT_PUBLIC_API_SUNAT_TOKEN}`,
+    `${process.env.NEXT_PUBLIC_LINK_API_SUNAT}/${documentType}/${documentNumber}?token=${process.env.NEXT_PUBLIC_API_SUNAT_TOKEN}`,
   );
 
   if (response.ok) {
