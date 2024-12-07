5ba99758f3802cdeb13723b4c4e63ec1db0aa5bc
EleoRomario <eleo.romario@gmail.com>
2024-12-07 11:30:39 -0500

    feat: agregar soporte para mensajes de WhatsApp en inglés y español

--- src/components/social/Whatsapp.tsx
+++ src/components/social/Whatsapp.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { WhatsApp } from "@/assets/icons";
+import { useTranslations } from "next-intl";
 
 export default function WhatsAppButton() {
   const phoneNumber = "123456789"; // Reemplaza esto con tu número de WhatsApp
-  const message = "Hola, me gustaría obtener más información."; // Mensaje predefinido (opcional)
+  const t = useTranslations("whatsapp");
 
   const handleClick = () => {
-    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(t("text"))}`;
     window.open(url, "_blank");
   };
 
