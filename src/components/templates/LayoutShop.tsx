56e964276970b8c437a1b49c0f5311b785a093ef
EleoRomario <eleo.romario@gmail.com>
2024-12-10 14:48:36 -0500

    feat: actualizar el fondo del layout de la tienda utilizando una imagen de relación

--- src/components/templates/LayoutShop.tsx
+++ src/components/templates/LayoutShop.tsx
@@ -1,3 +1,4 @@
+import Ratio from "@/assets/images/ratio.png";
 import { ReactNode } from "react";
 
 import WhatsAppButton from "@/components/social/Whatsapp";
@@ -5,7 +6,6 @@ import WhatsAppButton from "@/components/social/Whatsapp";
 import { Footer } from "./footer/Footer";
 import { LanguageSelector } from "./navbar/LanguageSelector";
 import { Navbar } from "./navbar/Navbar";
-
 interface LayoutShopProps {
   children: ReactNode;
 }
@@ -13,7 +13,14 @@ interface LayoutShopProps {
 export function LayoutShop({ children }: LayoutShopProps) {
   return (
     <div className="relative overflow-hidden rounded-2xl bg-white">
-      <div className="pointer-events-none fixed inset-0 z-10 h-full w-full rounded-none opacity-[0.08] [background-image:url('https://framerusercontent.com/images/rR6HYXBrMmX4cRpXfXUOvpvpB0.png');] [background-repeat:repeat;] [background-size:_109px;]"></div>
+      <div
+        className="pointer-events-none fixed inset-0 z-10 h-full w-full rounded-none opacity-[0.08]"
+        style={{
+          backgroundSize: "109px",
+          backgroundImage: `url(${Ratio.src})`,
+          backgroundRepeat: "repeat",
+        }}
+      ></div>
       <Navbar />
       <main className="py-40">{children}</main>
       <LanguageSelector />
