2b5c673f2a6401885f7410c3566c63e748e733ad
EleoRomario <eleo.romario@gmail.com>
2024-12-10 17:28:38 -0500

    feat: agregar componente HeroVideoDialog y actualizar el taller con videos interactivos

--- src/components/home/ClassWorkshop.tsx
+++ src/components/home/ClassWorkshop.tsx
@@ -1,61 +1,111 @@
+import PlaceholderChocolate from "@/assets/images/placeholder-chocolate.webp";
+import PlaceholderCoffe from "@/assets/images/placeholder-coffe.webp";
 import ChaqchaoWorkshop01 from "@/assets/images/workshop_01.webp";
 import ChaqchaoWorkshop02 from "@/assets/images/workshop_02.webp";
-import ChaqchaoWorkshop03 from "@/assets/images/workshop_03.webp";
 import Image from "next/image";
 
-import { cn } from "@/lib/utils";
-
+import { BentoGrid } from "../core/BentoGrid";
+import { HeroVideoDialog } from "../core/HeroVideoDialog";
 import { InView } from "../core/InView";
 
 export const ClassWorkshop = () => {
-  const images = [
-    {
-      src: ChaqchaoWorkshop01.src,
-      alt: "Chaqchao Workshop 01",
-    },
-    {
-      src: ChaqchaoWorkshop02.src,
-      alt: "Chaqchao Workshop 02",
-    },
-    {
-      src: ChaqchaoWorkshop03.src,
-      alt: "Chaqchao Workshop 03",
-    },
-  ];
-
   return (
-    <div className="mx-auto my-12 grid w-full max-w-7xl grid-flow-dense grid-cols-2 gap-6 rounded-md p-4">
-      {images.map(({ src, alt }, idx) => (
+    // <div className="mx-auto my-12 grid w-full max-w-7xl grid-flow-dense grid-cols-2 gap-6 rounded-md p-4">
+    <BentoGrid className="h-full auto-rows-auto grid-cols-3 grid-rows-2">
+      <div className="col-span-2 col-start-1 row-start-1 auto-rows-auto">
         <InView
-          key={idx}
           variants={{
             hidden: {
               opacity: 0,
-              translateX: (idx === 1 && 100) || 0,
-              translateY: idx === 0 ? -100 : idx === 2 ? 100 : 0,
+              translateY: -100,
             },
             visible: {
-              translateX: 0,
-              translateY: (idx === 0 && 0) || 0,
+              translateY: 0,
               opacity: 1,
             },
           }}
           viewOptions={{ margin: "0px 0px -200px 0px" }}
           transition={{ duration: 1, ease: "easeInOut" }}
-          className={cn("col-span-1 grid-flow-dense overflow-hidden", {
-            "row-span-2": idx === 0,
-            "row-span-1": idx !== 0,
-          })}
+          className="h-full w-full"
         >
-          <Image
-            className="h-full rounded-3xl object-cover"
-            width={400}
-            height={400}
-            src={src}
-            alt={alt}
+          <HeroVideoDialog
+            thumbnailSrc={PlaceholderCoffe.src}
+            videoSrc="https://pub-843b15cc9c5d4932b34855ee68a2f5be.r2.dev/cafe.mp4"
+            animationStyle="from-center"
+            thumbnailAlt="Chaqchao Chocolate"
           />
         </InView>
-      ))}
-    </div>
+      </div>
+      <div className="col-start-3 row-start-1 auto-rows-auto">
+        <InView
+          variants={{
+            hidden: {
+              opacity: 0,
+              translateX: 100,
+            },
+            visible: {
+              translateX: 0,
+              opacity: 1,
+            },
+          }}
+          viewOptions={{ margin: "0px 0px -200px 0px" }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="h-full w-full"
+        >
+          <Image
+            src={ChaqchaoWorkshop01}
+            alt="Christian Church, Eastern Europe"
+            className="h-full w-full rounded-3xl object-cover"
+          />
+        </InView>
+      </div>
+      <div className="col-start-1 row-start-2 auto-rows-auto">
+        <InView
+          variants={{
+            hidden: {
+              opacity: 0,
+              translateX: -100,
+            },
+            visible: {
+              translateX: 0,
+              opacity: 1,
+            },
+          }}
+          viewOptions={{ margin: "0px 0px -200px 0px" }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="h-full w-full"
+        >
+          <Image
+            src={ChaqchaoWorkshop02}
+            alt="Christian Church, Eastern Europe"
+            className="h-full w-full rounded-3xl object-cover object-right"
+          />
+        </InView>
+      </div>
+      <div className="col-span-2 col-start-2 row-start-2 auto-rows-auto">
+        <InView
+          variants={{
+            hidden: {
+              opacity: 0,
+              translateY: 100,
+            },
+            visible: {
+              translateY: 0,
+              opacity: 1,
+            },
+          }}
+          viewOptions={{ margin: "0px 0px -200px 0px" }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="h-full w-full"
+        >
+          <HeroVideoDialog
+            thumbnailSrc={PlaceholderChocolate.src}
+            videoSrc="https://pub-843b15cc9c5d4932b34855ee68a2f5be.r2.dev/chocolate.mp4"
+            animationStyle="from-center"
+            thumbnailAlt="Chaqchao Chocolate"
+          />
+        </InView>
+      </div>
+    </BentoGrid>
   );
 };
