052fbc8cc2154c81ecf80e4882e8298caad57c9e
EleoRomario <eleo.romario@gmail.com>
2025-02-05 09:20:33 -0500

    feat: envolver la página de checkout en el componente LayoutShop y ajustar estilos de botones

--- src/app/cart/checkout/page.tsx
+++ src/app/cart/checkout/page.tsx
@@ -1,13 +1,16 @@
 import { CartTitle } from "@/components/cart/CartTitle";
 import { Checkout } from "@/components/cart/Checkout";
+import { LayoutShop } from "@/components/templates/LayoutShop";
 
 export default function PageCart() {
   return (
-    <section className="container mx-auto flex flex-col gap-10 py-10">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <CartTitle />
-      </div>
-      <Checkout />
-    </section>
+    <LayoutShop>
+      <section className="container mx-auto flex flex-col gap-10 py-10">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <CartTitle />
+        </div>
+        <Checkout />
+      </section>
+    </LayoutShop>
   );
 }
