2191ad798aff6942e3de849bf8359bad04957601
Camila <camila.rodriguezbrown@ucsm.edu.pe>
2025-02-08 12:56:29 -0500

    feat(checkout-steps): implementar opción de envíos a otra ciudad en el carrito de compras

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -20,6 +20,7 @@ export type CheckoutCart = {
   customerEmail: string;
   customerPhone: string;
   someonePickup: boolean;
+  shippingToAnotherCity: boolean;
   comments?: string;
   pickupTime: Date;
   clientId?: string;
