0556c111f60f78979275db84e55f534a0179241e
Camila <camila.rodriguezbrown@ucsm.edu.pe>
2025-02-20 13:27:02 -0500

    feat: agregar mensaje emergente de bienvenida y opción de contacto por WhatsApp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import { ProductsFilters } from "@/components/categories/ProductsFilters";
+"use client";
+import {
+  MensajeEmergente,
+  ProductsFilters,
+} from "@/components/categories/ProductsFilters";
 import { LayoutShop } from "@/components/templates/LayoutShop";
 
 export default function Home() {
   return (
     <LayoutShop>
       <ProductsFilters />
+      <MensajeEmergente />
     </LayoutShop>
   );
 }
