5b8e1c105236c7d75f077ca1a6be47a996cea286
eleoromario <eleo.romario@gmail.com>
2025-03-31 14:04:44 -0500

    refactor(login): update terms link to point to PDF document

--- src/components/account/login/FormLogin.tsx
+++ src/components/account/login/FormLogin.tsx
@@ -153,7 +153,11 @@ export const FormLogin = () => {
             </p>
             <p className="mt-4 text-center text-xs text-gray-500">
               {t("terms")}
-              <Link href="/terms" className="ml-1 font-bold hover:text-primary">
+              <Link
+                href="/Terminos_y_Condiciones.pdf"
+                target="_blank"
+                className="ml-1 font-bold hover:text-primary"
+              >
                 {t("termsLink")}
               </Link>
             </p>
