ea2859e199775c73644fca3b3bdacd15d24590a3
eleoromario <eleo.romario@gmail.com>
2025-04-08 11:02:35 -0500

    fix(workshops): reemplazar componente Image de Next.js por etiqueta img para mejorar la consistencia y el rendimiento

--- src/app/(class)/workshops/page.tsx
+++ src/app/(class)/workshops/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Image from "next/image";
-
 import { WorkshopInfo } from "@/components/classes/WorkshopInfo";
 import WorkshopSelectDate from "@/components/classes/WorkshopSelectDate";
 
@@ -9,35 +7,26 @@ export default function ReservationPage() {
   return (
     <section className="relative">
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-[1fr_200px_200px]">
-        <Image
+        <img
           src="/images/workshop_01.webp"
           alt="chaqchao factory"
           width={1920}
           height={1080}
           className="w-full"
-          priority
-          placeholder="blur"
-          blurDataURL="/images/workshop_01.jpg"
         />
-        <Image
+        <img
           src="/images/workshop_02.webp"
           alt="chaqchao factory"
           width={500}
           height={800}
           className="hidden h-full object-cover object-center sm:flex"
-          priority
-          placeholder="blur"
-          blurDataURL="/images/workshop_02.jpg"
         />
-        <Image
+        <img
           src="/images/workshop_03.webp"
           alt="chaqchao factory"
           width={1920}
           height={1080}
           className="hidden h-full object-cover object-center sm:flex"
-          priority
-          placeholder="blur"
-          blurDataURL="/images/workshop_03.jpg"
         />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2">
